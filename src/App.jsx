d7b2e3d81fc937cb83a52fecef14037a366ef976
Hassan <hassan.seedat07@gmail.com>
2022-06-28 16:08:07 +0100

    browser router imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import NavBar from "./components/NavBar/NavBar";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
+
+    
     <div className="App">
-    <NavBar/>
+    
+      <NavBar/>
+
     </div>
+    
   );
 }
 
