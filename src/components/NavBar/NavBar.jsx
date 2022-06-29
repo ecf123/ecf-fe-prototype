0951736682ed8682d5bf8d8e6a068d800d195d3c
sam <sam_pridmore@hotmail.co.uk>
2022-06-29 15:40:07 +0100

    added test class to access svg color

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./NavBar.scss"
 import { Link } from "react-router-dom";
 
@@ -8,17 +8,26 @@ import branches from "../../assets/images/forest-icon.svg";
 import profile from  "../../assets/images/profile-settings.svg";
 
 
-const NavBar = () => {
 
+const NavBar = () => {
+  
+  const [changeColor, setChangecolor] = useState();
+  
+  
+  const handleSelected = (event) => {
+    // console.log(event.target.classList);
+    console.log(event);
+    
+  }
 
   return (
 
     
     <div className='nav-bar'>
-        <Link to="/"><button className="nav-bar__buttons"><img src={home} alt="Home icon" /></button></Link>
-        <Link to="/Library"><button className="nav-bar__buttons"><img src={library} alt="Resources icon icon" /></button></Link>
-        <Link to="/Branches"><button className="nav-bar__buttons"><img src={branches} alt="Learning Branch" /></button></Link>
-        <Link to="/Profile"><button className="nav-bar__buttons"><img src={profile} alt="User Profile icon" /></button></Link>
+        <Link to="/"><button onClick={handleSelected} className="nav-bar__buttons"><img className='nav-bar__test' src={home} alt="Home icon" /></button></Link>
+        <Link to="/Library"><button onClick={handleSelected} className="nav-bar__buttons"><img src={library} alt="Resources icon icon" /></button></Link>
+        <Link to="/Branches"><button onClick={handleSelected} className="nav-bar__buttons"><img src={branches} alt="Learning Branch" /></button></Link>
+        <Link to="/Profile"><button onClick={handleSelected} className="nav-bar__buttons"><img src={profile} alt="User Profile icon" /></button></Link>
     </div>
     
   )
