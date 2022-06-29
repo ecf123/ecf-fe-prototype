3f442743de332ab71dcf23c2c1a552b6fbf0d8fb
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2022-06-29 15:03:58 +0100

    Added final tyest to take text input

--- src/components/InputField/InputField.test.js
+++ src/components/InputField/InputField.test.js
@@ -1,5 +1,6 @@
 import InputField from "./InputField";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 describe("initial tests for ECF client project input field component", () => {
   it("should render the form on the page", () => {
@@ -10,5 +11,17 @@ describe("initial tests for ECF client project input field component", () => {
 
     expect(input).toBeInTheDocument;
     expect(label).toBeInTheDocument;
+  });
+
+  it("should take a text input", () => {
+    render(<InputField input="label" />);
+
+    const input = screen.getByRole("textbox");
+
+    const inputText = userEvent.type(input, "hello world");
+
+    expect(input).toBeInTheDocument;
+
+    expect(screen.getByRole("textbox")).toHaveValue("hello world");
   });
 });
