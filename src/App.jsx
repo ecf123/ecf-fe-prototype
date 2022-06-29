965de2a2f2711f11d61ba536707eea55872eafb9
Mark Lawson <mark@walkonmars.co.uk>
2022-06-29 20:41:46 +0100

    just styling of progress bar left to do

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-// TEMPORARY
+import "./App.scss";
+
+// TEMPORARY //////////////////
 import PathwaysCard from "./components/PathwaysCard/PathwaysCard";
 
-const card = {header: "Financial Forest", topics: "Banking, Investments", percentage: 90};
+const card = {header: "Financial Forest", topics: "Banking, Investments", percentage: 80};
+
+//////////////////////////////
 
 const App = () => {
   return (
--- src/components/PathwaysCard/PathwaysCard.jsx
+++ src/components/PathwaysCard/PathwaysCard.jsx
@@ -5,14 +5,17 @@ import {CircularProgressbar} from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 const PathwaysCard = ({card}) => {
   const {header, topics, percentage} = card;
+
   return (
     <div className="card">
       <div className="card__progress">
-        <CircularProgressbar className="card__progress--bar" value={percentage} text={`90`} />
+        <CircularProgressbar className="card__progress--bar" value={percentage} text={percentage} />
       </div>
-      <img className="card__image" src={image} alt="" />
-      <h1 className="card__header">{header}</h1>
-      <h2 className="card__topics">{topics}</h2>
+      <div className="card__content">
+        <img className="card__content-image" src={image} alt="" />
+        <h1 className="card__content-header">{header}</h1>
+        <h2 className="card__content-topics">{topics}</h2>
+      </div>
     </div>
   );
 };
