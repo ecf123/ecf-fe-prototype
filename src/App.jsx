3ad3a056adf0dd5f7171d5ce5b25542f1ad2de94
isobelleanna <67749649+isobelleanna@users.noreply.github.com>
2022-06-29 14:45:24 +0100

    import button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
+import Button from "./components/Button/Button";
 const App = () => {
-  return (
-    <div className="App">
-
-    </div>
-  );
-}
+  return <div className="App"></div>;
+};
 
 export default App;
