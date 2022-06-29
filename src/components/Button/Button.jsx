d892d9abbdfd2f4ad16468d2c1c6326521e4940c
isobelleanna <67749649+isobelleanna@users.noreply.github.com>
2022-06-29 15:00:40 +0100

    adjust styling

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./Button.scss";
 
-const Button = ({ buttonText, isSecondary, onClick, buttonType }) => {
+const Button = ({ buttonText, isSecondary, onClickButton, buttonType }) => {
   let buttonStyle = "button";
 
   if (isSecondary) {
     buttonStyle += " button__secondary";
   }
   return (
-    <button className={buttonStyle} type={buttonType} onClick={onClick}>
+    <button className={buttonStyle} type={buttonType} onClick={onClickButton}>
       {buttonText}
     </button>
   );
