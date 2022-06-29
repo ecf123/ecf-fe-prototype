7d1925d08695d1d0001f59fedf3d9d5cefec9ca4
HPTR <76168685+HPTR@users.noreply.github.com>
2022-06-29 15:20:18 +0100

    Remove test rendering from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import InputField from "./components/InputField/InputField";
+
 
 const App = () => {
   return (
     <div className="App">
-      <InputField
-        inputName="input-name"
-        inputType="input-type"
-        placeholder="Email Address"
-        label="label"
-      />
+
     </div>
   );
 };
