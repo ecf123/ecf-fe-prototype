6da42e529962363d307de48c20c5e51fc9a387ba
HPTR <76168685+HPTR@users.noreply.github.com>
2022-06-29 16:24:04 +0100

    Remove unnecessary container

--- src/components/InputField/InputField.jsx
+++ src/components/InputField/InputField.jsx
@@ -5,17 +5,15 @@ const InputField = (props) => {
   const { inputName, inputType, placeholder, label } = props;
   return (
     <div className="input">
-      <div className="new-input">
-        <label className="input__label" htmlFor={inputName}>
-          {label}
-        </label>
-        <input
-          className="input__input"
-          type={inputType}
-          name={inputName}
-          placeholder={placeholder}
-        />
-      </div>
+      <label className="input__label" htmlFor={inputName}>
+        {label}
+      </label>
+      <input
+        className="input__input"
+        type={inputType}
+        name={inputName}
+        placeholder={placeholder}
+      />
     </div>
   );
 };
