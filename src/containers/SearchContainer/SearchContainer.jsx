2ff6729c0fd6317a8d4aa4862a807b4212d20ff4
rachel2bester <rachel2bester@gmail.com>
2022-06-29 16:39:13 +0200

    finished search  bar

--- src/containers/SearchContainer/SearchContainer.jsx
+++ src/containers/SearchContainer/SearchContainer.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
-import SearchBar from '../../components/SearchBar/SearchBar'
+import React, { useEffect, useState } from "react";
+import SearchBar from "../../components/SearchBar/SearchBar";
+import "./SearchContainer.scss";
 
-const SearchContainer = () => {
-
+const SearchContainer = ({ title }) => {
+  // dummy array
   const arr = [
     "apple",
     "orange",
@@ -14,39 +15,59 @@ const SearchContainer = () => {
     "lemon",
     "lime",
     "watermelon",
-    "mango"
-  ]
-
- 
+    "mango",
+  ];
 
   const [searchTerm, setSearchTerm] = useState("");
   const [arrayJsx, setArrayJsx] = useState([]);
 
-  setArrayJsx(arr.map((element, index) =>{
-    return <p key={index}>{element}</p>
-  }))
-  
+  useEffect(() => {
+    setArrayJsx(
+      arr.map((element, index) => {
+        return <p key={index}>{element}</p>;
+      })
+    );
+  }, []);
+
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.search.value);
-    console.log(event.target.search.value)
     event.preventDefault();
-    
-    const filterArray = arr.filter((element)=> {
-      return element.includes(event.target.search.value);
 
-    })
-   
+    const filterArray = arr.filter((element) => {
+      return element
+        .toLowerCase()
+        .includes(event.target.search.value.toLowerCase());
+    });
 
-  }
-
+    setArrayJsx(
+      filterArray.map((element, index) => {
+        return <p key={index}>{element}</p>;
+      })
+    );
+  };
 
   return (
-    <div>
-      <SearchBar handleChange = {handleSearchChange}/>
-      {searchTerm &&  <h1>Search results for "{searchTerm}"</h1>}
-      {arrayJsx}
+    <div className="search-container">
+      <SearchBar handleChange={handleSearchChange} />
+      {arrayJsx.length ? (
+        searchTerm ? (
+          <h1 className="search-container__heading">
+            Search results for "{searchTerm}"
+          </h1>
+        ) : (
+          <div className="search-container__titles">
+            <h1 className="search-container__heading">{title}</h1>
+            <h2 className="search-container__view-button">View All</h2>
+          </div>
+        )
+      ) : (
+        <h1 className="search-container__heading">
+          Nothing matched your search.
+        </h1>
+      )}
+      <div className="search-container__list">{arrayJsx}</div>
     </div>
-  )
-}
+  );
+};
 
-export default SearchContainer
+export default SearchContainer;
