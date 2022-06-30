6b682c4df068bfbf93d675033c77ad314d190fd3
HPTR <76168685+HPTR@users.noreply.github.com>
2022-06-30 21:07:03 +0100

    Add SocialLogin test file

---
+++ src/components/SocialLogin/SocialLogin.test.js
@@ -0,0 +1,7 @@
+import SocialLogin from "./SocialLogin";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+describe("initial tests for login page", () => {
+  
+});
