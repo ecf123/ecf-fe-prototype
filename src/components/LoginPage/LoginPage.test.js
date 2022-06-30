906f6fbc63e2691c38ea1e810a7f63519ef786d0
HPTR <76168685+HPTR@users.noreply.github.com>
2022-06-30 21:01:11 +0100

    Add LoginPage test file

---
+++ src/components/LoginPage/LoginPage.test.js
@@ -0,0 +1,7 @@
+import InputField from "./InputField";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+describe("initial tests for login page", () => {
+  
+});
