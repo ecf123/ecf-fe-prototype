0f1e1edca4041f20b87f1541f2ef40f4bb22da2b
Mark Lawson <mark@walkonmars.co.uk>
2022-06-30 15:34:59 +0100

    merged changes into branch 20

--- src/components/PathwaysCard/PathwaysCard.jsx
+++ src/components/PathwaysCard/PathwaysCard.jsx
@@ -1,15 +1,25 @@
 import React from "react";
+import {CircularProgressbar, buildStyles} from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 import "./PathwaysCard.scss";
 import image from "../../assets/images/financial-forest.svg";
-import {CircularProgressbar} from "react-circular-progressbar";
-import "react-circular-progressbar/dist/styles.css";
+
 const PathwaysCard = ({card}) => {
   const {header, topics, percentage} = card;
 
   return (
     <div className="card">
       <div className="card__progress">
-        <CircularProgressbar className="card__progress--bar" value={percentage} text={percentage} />
+        <CircularProgressbar
+          className="card__progress-bar"
+          value={percentage}
+          text={percentage}
+          styles={buildStyles({
+            textColor: "red",
+            pathColor: "turquoise",
+            trailColor: "gold",
+          })}
+        />
       </div>
       <div className="card__content">
         <img className="card__content-image" src={image} alt="" />
