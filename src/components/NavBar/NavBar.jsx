9e3695e91eaef29450f08428899d78ac83726861
sam <sam_pridmore@hotmail.co.uk>
2022-06-30 15:17:48 +0100

    colors of icons changing when changing screens

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,33 +1,20 @@
-import React, {useState} from 'react'
+import React from 'react'
 import "./NavBar.scss"
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import home from "../../assets/images/home-icon.svg";
 import library from "../../assets/images/library.svg";
 import branches from "../../assets/images/forest-icon.svg";
 import profile from  "../../assets/images/profile-settings.svg";
 
-
-
 const NavBar = () => {
   
-  const [changeColor, setChangecolor] = useState();
-  
-  
-  const handleSelected = (event) => {
-    // console.log(event.target.classList);
-    console.log(event);
-    
-  }
-
   return (
-
-    
     <div className='nav-bar'>
-        <Link to="/"><button onClick={handleSelected} className="nav-bar__buttons"><img className='nav-bar__test' src={home} alt="Home icon" /></button></Link>
-        <Link to="/Library"><button onClick={handleSelected} className="nav-bar__buttons"><img src={library} alt="Resources icon icon" /></button></Link>
-        <Link to="/Branches"><button onClick={handleSelected} className="nav-bar__buttons"><img src={branches} alt="Learning Branch" /></button></Link>
-        <Link to="/Profile"><button onClick={handleSelected} className="nav-bar__buttons"><img src={profile} alt="User Profile icon" /></button></Link>
+        <NavLink to="/" className={({isActive})=> (isActive ? "nav-bar__link-active" : "nav-bar__link")}><img src={home} alt="Home icon"/></NavLink>
+        <NavLink to="/Library" className={({isActive})=> (isActive ? "nav-bar__link-active" : "nav-bar__link")}><img src={library} alt="Resources icon" /></NavLink>
+        <NavLink to="/Branches" className={({isActive})=> (isActive ? "nav-bar__link-active" : "nav-bar__link")}><img src={branches} alt="Learning Branch icon" /></NavLink>
+        <NavLink to="/Profile" className={({isActive})=> (isActive ? "nav-bar__link-active" : "nav-bar__link")}><img src={profile} alt="User Profile icon" /></NavLink>
     </div>
     
   )
