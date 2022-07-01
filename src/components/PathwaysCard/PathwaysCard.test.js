f56000598109effceba996130f3edcf89c93ecde
Mark Lawson <mark@walkonmars.co.uk>
2022-07-01 15:51:16 +0100

    testing

--- src/components/PathwaysCard/PathwaysCard.test.js
+++ src/components/PathwaysCard/PathwaysCard.test.js
@@ -6,13 +6,23 @@ describe("basic tests for card component", () => {
     const card = {header: "Financial Forest", topics: "Banking, Investments"};
 
     render(<PathwaysCard card={card} />);
-    const div = screen.toHaveClass("card");
+    const div = screen.getByTestId("1");
     expect(div).toBeInTheDocument();
   });
 
-  // it("should render the progress bar", () => {
-  //   const card = {header: "Financial Forest", topics: "Banking, Investments"};
+  it("should render the progress bar", () => {
+    const card = {header: "Financial Forest", topics: "Banking, Investments"};
 
-  //   render(< PathwaysCard/>);
-  // });
+    render(<PathwaysCard card={card} />);
+    const div = screen.getByTestId("2");
+    expect(div).toBeInTheDocument();
+  });
+
+  it("should render the image", () => {
+    const card = {header: "Financial Forest", topics: "Banking, Investments"};
+
+    render(<PathwaysCard card={card} />);
+    const img = screen.getByRole("img");
+    expect(img).toBeInTheDocument();
+  });
 });
