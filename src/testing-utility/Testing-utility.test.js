fa366f1000ebc37fbb9e4d14d782c5eea643bf87
sam <sam_pridmore@hotmail.co.uk>
2022-07-01 15:04:43 +0100

    commit with notes

--- src/testing-utility/Testing-utility.test.js
+++ src/testing-utility/Testing-utility.test.js
@@ -1,6 +1,13 @@
 import { render } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
 
+
+// ui = Component that needs rendering
+// 2nd parameter is not necessary does not need passing in , It has a default value. Can be overridden. 
+
+// Call customerRender in your testing file instead of calling -> render() 
+
+
 export const customRender = (ui, useRouting = true) => {
   // wrap components in routing if requested
   const uiResult = useRouting && wrapWithRouting(ui);
