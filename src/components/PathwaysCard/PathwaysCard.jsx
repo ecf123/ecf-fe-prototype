ad2bb360ad6c0bc0ca4c04547a4b90d3c32ca31d
Gavin Stirling <gavinstir@gmail.com>
2022-07-01 16:33:08 +0100

    Allowed overlay change functionality

--- src/components/PathwaysCard/PathwaysCard.jsx
+++ src/components/PathwaysCard/PathwaysCard.jsx
@@ -1,35 +1,84 @@
-import {React, useState} from "react";
-import {NavLink} from "react-router-dom";
-import {CircularProgressbar, buildStyles} from "react-circular-progressbar";
+import { React, useState } from "react";
+
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import "./PathwaysCard.scss";
 import image from "../../assets/images/financial-forest.svg";
 
-const PathwaysCard = ({card, percentage}) => {
-  const {header, topics} = card;
+const PathwaysCard = ({ card, percentage }) => {
+    const { header, topics } = card;
+    const [overlayActive, setOverlayActive] = useState(false);
 
-  return (
-    <div className="card" data-testid="1">
-      <div className="card__progress" data-testid="2">
-        <CircularProgressbar
-          className="card__progress-bar"
-          value={percentage}
-          text={percentage}
-          strokeWidth={10}
-          styles={buildStyles({
-            textColor: "#3b5165",
-            pathColor: "#b4ddc0",
-            trailColor: "#e9e9e9",
-            textSize: "22px",
-          })}
-        />
-      </div>
-      <div className="card__content">
-        <img className="card__content-image" src={image} alt="" />
-        <h1 className="card__content-header">{header}</h1>
-        <h2 className="card__content-topics">{topics}</h2>
-      </div>
-    </div>
+    const handleClick = () => {
+        setOverlayActive(true);
+        console.log("Switched to overlay view");
+    };
+
+    const PathwayCardJSX = (
+        <>
+            <div className="card" data-testid="1" onClick={handleClick}>
+                    <div className="card__progress" data-testid="2">
+                        <CircularProgressbar
+                            className="card__progress-bar"
+                            value={percentage}
+                            text={percentage}
+                            strokeWidth={10}
+                            styles={buildStyles({
+                                textColor: "#3b5165",
+                                pathColor: "#b4ddc0",
+                                trailColor: "#e9e9e9",
+                                textSize: "22px",
+                            })}
+                        />
+                    </div>
+                    <div className="card__content">
+                        <img
+                            className="card__content-image"
+                            src={image}
+                            alt=""
+                        />
+                        <h1 className="card__content-header">{header}</h1>
+                        <h2 className="card__content-topics">{topics}</h2>
+                    </div>
+            </div>
+        </>
+    );
+
+    const PathwayOverlayJSX = (
+      <>
+          <div className="card" data-testid="overlay">
+              Pathway overlay
+          </div>
+      </>
   );
+
+    return (
+        <>
+        {overlayActive ? PathwayOverlayJSX : PathwayCardJSX}
+        </>
+        // <div className="card" data-testid="1" onClick={handleClick}>
+        //   <NavLink to="/">
+        //   <div className="card__progress" data-testid="2">
+        //     <CircularProgressbar
+        //       className="card__progress-bar"
+        //       value={percentage}
+        //       text={percentage}
+        //       strokeWidth={10}
+        //       styles={buildStyles({
+        //         textColor: "#3b5165",
+        //         pathColor: "#b4ddc0",
+        //         trailColor: "#e9e9e9",
+        //         textSize: "22px",
+        //       })}
+        //     />
+        //   </div>
+        //   <div className="card__content">
+        //     <img className="card__content-image" src={image} alt="" />
+        //     <h1 className="card__content-header">{header}</h1>
+        //     <h2 className="card__content-topics">{topics}</h2>
+        //   </div>
+        //   </NavLink>
+        // </div>
+    );
 };
 export default PathwaysCard;
--- src/components/PathwaysCard/PathwaysCard.test.js
+++ src/components/PathwaysCard/PathwaysCard.test.js
@@ -1,4 +1,5 @@
 import {render, screen} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import PathwaysCard from "./PathwaysCard";
 
 describe("basic tests for card component", () => {
@@ -24,5 +25,17 @@ describe("basic tests for card component", () => {
     render(<PathwaysCard card={card} />);
     const img = screen.getByRole("img");
     expect(img).toBeInTheDocument();
+  });
+});
+
+describe("Changing to overlay view", () => {
+  it("Should render the card", () => {
+    const card = {header: "Financial Forest", topics: "Banking, Investments"};
+
+    render(<PathwaysCard card={card} />);
+    const div = screen.getByTestId("1");
+    userEvent.click(div);
+    const overlay = screen.getByTestId("overlay");
+    expect(overlay).toBeInTheDocument();
   });
 });
