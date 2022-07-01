60fd8278b134285e50a0481b6063a5126ed406a1
suarezfer1712 <suarezfer1712@gmail.com>
2022-07-01 14:16:01 +0100

    added some props

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import SearchContainer from "./containers/SearchContainer/SearchContainer";
+import Button from "./components/Button/Button";
+
+const App = () => {
+
+  const handleOnClick = () => {
+    console.log(1);
+  };
+  
+
+
+    return (
+        <div className="App">
+            <SearchContainer title="Pathways"/>
+          
+            <Button
+              buttonText="START PATHWAYS"
+              buttonType="submit"
+              isSecondary={false}
+              onClickButton={handleOnClick}
+            />
+            <Button
+              buttonText="SIGN IN"
+              buttonType="submit"
+              isSecondary={true}
+              onClickButton={handleOnClick}
+            />
+        </div>
+
+    );
+}
+
+
+
+export default App;
---
+++ src/components/MarketPlaceCard/MarketPlaceCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import trophy from "./../../assets/images/trophy-icon.svg"
+
+
+const MarketPlaceCard = (props) => {
+  const {img,title,trophies, score}=props;
+  return (
+    <div>MarketPlaceCard</div>
+  )
+}
+
+export default MarketPlaceCard
