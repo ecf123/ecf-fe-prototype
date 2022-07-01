6df8e3de836f7925b3640c492c6f48949b8243ec
HPTR <76168685+HPTR@users.noreply.github.com>
2022-07-01 15:12:58 +0100

    Add SocialLogin tests

--- src/components/SocialLogin/SocialLogin.test.js
+++ src/components/SocialLogin/SocialLogin.test.js
@@ -3,5 +3,19 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 describe("initial tests for social login button", () => {
-  
+    it("should render a button", () => {
+        render(<SocialLogin />);
+        const button = screen.getByRole("button");
+        expect(button).toBeInTheDocument()
+        expect(button).not.toBeNull()
+    });
+
+    it("should render text passed in the icon prop", () => {
+        render(<SocialLogin icon="Test" />);
+        const buttonText = screen.getByText("Test");
+        expect(buttonText).toBeInTheDocument()
+        expect(buttonText).not.toBeNull()
+    });
+
+    // Further tests can be added once Authorisation is implemented
 });
