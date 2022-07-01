78173cf8e4d5aee238937f678c6b00de27920ed0
HPTR <76168685+HPTR@users.noreply.github.com>
2022-07-01 15:54:10 +0100

    Remove test rendering of SignIn component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 import SignIn from "./containers/SignIn/SignIn";
-=======
 import Home from "./containers/Home/Home";
 import userProfile from "./assets/data/dummyUserData"
 import "./App.scss";
 import SearchContainer from "./containers/SearchContainer/SearchContainer";
 import Button from "./components/Button/Button";
->>>>>>> 06b8739ccdf3dbbbfd9dd4f60da1da7a46bd413e
 
 const App = () => {
 
@@ -16,36 +13,25 @@ const App = () => {
   
 
 
-//     return (
-//         <div className="App">
-//             <SearchContainer title="Pathways"/>
+    return (
+        <div className="App">
+            <SearchContainer title="Pathways"/>
           
-//             <Button
-//               buttonText="START PATHWAYS"
-//               buttonType="submit"
-//               isSecondary={false}
-//               onClickButton={handleOnClick}
-//             />
-//             <Button
-//               buttonText="SIGN IN"
-//               buttonType="submit"
-//               isSecondary={true}
-//               onClickButton={handleOnClick}
-//             />
-//         </div>
+            <Button
+              buttonText="START PATHWAYS"
+              buttonType="submit"
+              isSecondary={false}
+              onClickButton={handleOnClick}
+            />
+            <Button
+              buttonText="SIGN IN"
+              buttonType="submit"
+              isSecondary={true}
+              onClickButton={handleOnClick}
+            />
+        </div>
 
-//     );
-// }
-
-
-<<<<<<< HEAD
-  return (
-    <div className="App">
-      <SignIn welcomeMessage="Welcome Back" />
-    </div>
-  );
+    );
 };
 
-=======
->>>>>>> 06b8739ccdf3dbbbfd9dd4f60da1da7a46bd413e
 export default App;
