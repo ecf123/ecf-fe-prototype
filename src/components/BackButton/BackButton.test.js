e8aa1019432d934e2bd023818b4970950c0c80bc
HPTR <76168685+HPTR@users.noreply.github.com>
2022-07-01 15:50:24 +0100

    Add Routing test to BackButton

--- src/components/BackButton/BackButton.test.js
+++ src/components/BackButton/BackButton.test.js
@@ -1,4 +1,7 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Splash from "../../containers/Splash/Splash";
+import { customRender } from "../../testing-utility/Testing-utility";
 import BackButton from "./BackButton";
 
 describe("initial test for back button", () => {
@@ -7,5 +10,16 @@ describe("initial test for back button", () => {
         const button = screen.getByRole("button");
         expect(button).toBeInTheDocument()
         expect(button).not.toBeNull()
+    });
+
+    it("should direct to a specified routing path when clicked", () => {
+        customRender(<BackButton linkTo="/" />);
+        customRender(<Splash path="/" />)
+
+        const backButton = screen.getByRole("button");
+        userEvent.click(backButton);
+        const splashText = screen.getByText("Splash");
+
+        expect(splashText).toBeInTheDocument();
     });
 });
