02674d98e44eb4180fb4af0a8adbadf7745565bb
HPTR <76168685+HPTR@users.noreply.github.com>
2022-07-04 16:54:55 +0100

    Update a tag to Link tag

--- src/components/ContentHeader/ContentHeader.jsx
+++ src/components/ContentHeader/ContentHeader.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import "./ContentHeader.scss";
 
 const ContentHeader = ({title, link}) => {
-
-  //href attribute should be changed to 'to' when routing is implemented
-
   return (
     <div className="content-header">
         <h1 className="content-header__heading">{title}</h1>
-        <a className="content-header__anchor" href={link}>
+        <Link className="content-header__anchor" to={link}>
             View All
-        </a>
+        </Link>
     </div>
   )
 }
