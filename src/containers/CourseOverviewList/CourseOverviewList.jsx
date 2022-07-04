20fad704832e4f6b6b1765839e1fae67990cc6d1
isobelleanna <67749649+isobelleanna@users.noreply.github.com>
2022-07-04 16:59:32 +0100

    styled list container title

--- src/containers/CourseOverviewList/CourseOverviewList.jsx
+++ src/containers/CourseOverviewList/CourseOverviewList.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import CourseOverviewListItem from "../../components/CourseOverviewListItem/CourseOverviewListItem";
 //import PlayIcon from "../"
 import Redo from "../../assets/images/redo.svg";
+import "./CourseOverviewList.scss";
 
 const CourseOverviewList = ({ title }) => {
   return (
     <div>
       CourseOverviewList
-      <h1>{title}</h1>
+      <h1 className="list-container__title">{title}</h1>
       <div>
-        <CourseOverviewListItem image={Redo} title="The Role of Banking in The Financial System" duration="10" />
+        <CourseOverviewListItem
+          image={Redo}
+          title="The Role of Banking in The Financial System"
+          duration="10"
+        />
         <CourseOverviewListItem />
         <CourseOverviewListItem />
         <CourseOverviewListItem />
