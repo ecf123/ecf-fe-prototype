945e17c37d0361d92d6fceb19f7bf967d725ba71
isobelleanna <67749649+isobelleanna@users.noreply.github.com>
2022-07-04 16:33:35 +0100

    updated styling

--- src/components/CourseOverviewListItem/CourseOverviewListItem.jsx
+++ src/components/CourseOverviewListItem/CourseOverviewListItem.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import ClockIcon from "../../assets/images/clock-icon.svg";
+import "./CourseOverviewListItem.scss";
 
 const CourseOverviewListItem = ({ image, title, duration }) => {
   return (
     <div className="list-item">
-      <img className="list-item__icon" src={image} alt="" />
-      <h1 className="list-item__title">{title}</h1>
-      <div className="list-item__duration">
-        <img className="list-item__duration--icon" src={ClockIcon} alt="" />
-        <h2 className="list-item__duration--heading">{duration} minutes</h2>
+      <div className="list-item__icon-container">
+        <img className="list-item__icon" src={image} alt="" />
+      </div>
+      <div className="list-item__content">
+        <h1 className="list-item__title">{title}</h1>
+        <div className="list-item__duration">
+          <img className="list-item__duration--icon" src={ClockIcon} alt="" />
+          <h2 className="list-item__duration--heading">{duration} minutes</h2>
+        </div>
       </div>
     </div>
   );
