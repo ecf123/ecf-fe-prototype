293d219830a425a39f99cecfb6b0e45a0958df74
HPTR <76168685+HPTR@users.noreply.github.com>
2022-07-04 16:33:00 +0100

    Update comment location

--- src/components/ContentHeader/ContentHeader.jsx
+++ src/components/ContentHeader/ContentHeader.jsx
@@ -3,10 +3,11 @@ import "./ContentHeader.scss";
 
 const ContentHeader = ({title, link}) => {
 
+  //href attribute should be changed to 'to' when routing is implemented
+
   return (
     <div className="content-header">
         <h1 className="content-header__heading">{title}</h1>
-        //href attribute should be changed to 'to' when routing is implemented
         <a className="content-header__anchor" href={link}>
             View All
         </a>
