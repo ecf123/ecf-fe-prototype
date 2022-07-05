f19f705e84ca1780f9c4ae5d3b6a322e6e3cbe5e
Mark Lawson <mark@walkonmars.co.uk>
2022-07-05 16:57:02 +0100

    aded pathways card to carousel

--- src/components/PathwaysCard/PathwaysCard.jsx
+++ src/components/PathwaysCard/PathwaysCard.jsx
@@ -1,47 +1,42 @@
-import { React } from "react";
+import {React} from "react";
 
-import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import {CircularProgressbar, buildStyles} from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import "./PathwaysCard.scss";
 
+import card from "../../assets/data/dummyPathwayData";
 
-const PathwaysCard = ({ card, percentage }) => {
-    const { header, topics, image } = card;
+// {card, percentage} replace in ()
 
-    return (
-        <>
-            <div
-                className="card"
-                data-testid="pathwaycard"
-            >
-                <div
-                    className="card__progress"
-                    data-testid="circularprogressbar"
-                >
-                    <CircularProgressbar
-                        className="card__progress-bar"
-                        value={percentage}
-                        text={percentage}
-                        strokeWidth={10}
-                        styles={buildStyles({
-                            textColor: "#3b5165",
-                            pathColor: "#b4ddc0",
-                            trailColor: "#e9e9e9",
-                            textSize: "22px",
-                        })}
-                    />
-                </div>
-                <div className="card__content">
-                    <img
-                        className="card__content-image"
-                        src={image}
-                        alt={header}
-                    />
-                    <h1 className="card__content-header">{header}</h1>
-                    <h2 className="card__content-topics">{topics}</h2>
-                </div>
-            </div>
-        </>
-    );
+const PathwaysCard = () => {
+  const {header, topics, image} = card;
+
+  const percentage = 80;
+
+  return (
+    <>
+      <div className="card" data-testid="pathwaycard">
+        <div className="card__progress" data-testid="circularprogressbar">
+          <CircularProgressbar
+            className="card__progress-bar"
+            value={percentage}
+            text={percentage}
+            strokeWidth={10}
+            styles={buildStyles({
+              textColor: "#3b5165",
+              pathColor: "#b4ddc0",
+              trailColor: "#e9e9e9",
+              textSize: "22px",
+            })}
+          />
+        </div>
+        <div className="card__content">
+          <img className="card__content-image" src={image} alt={header} />
+          <h1 className="card__content-header">{header}</h1>
+          <h2 className="card__content-topics">{topics}</h2>
+        </div>
+      </div>
+    </>
+  );
 };
 export default PathwaysCard;
