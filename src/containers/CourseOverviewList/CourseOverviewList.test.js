afbd5dfdb796a1e027bc54789d68c5aba6431da9
isobelleanna <67749649+isobelleanna@users.noreply.github.com>
2022-07-05 17:01:25 +0100

    started testing

--- src/containers/CourseOverviewList/CourseOverviewList.test.js
+++ src/containers/CourseOverviewList/CourseOverviewList.test.js
@@ -1,10 +1,10 @@
 import {render, screen} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import CourseOverviewListItem from "./CourseOverviewListItem";
+import CourseOverviewList from "./CourseOverviewList";
 
 
 it('renders on the page', () => {
-    render(<CourseOverviewListItem />)
-    const list = screen.getByTestId("course-list-item");
+    render(<CourseOverviewList />)
+    const list = screen.getByTestId("course-overview-list");
     expect(list).toBeInTheDocument();
 })
