1243ee87657d60080e49f0e2c621c4e9b2893f06
rachel2bester <rachel2bester@gmail.com>
2022-07-05 15:31:10 +0200

    imported beautiful-skills-tree

---
+++ src/containers/SkillsTree/SkillsTree.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {
+  SkillTreeGroup,
+  SkillTree,
+  SkillProvider,
+  SkillType,
+  SkillGroupDataType,
+} from 'beautiful-skill-tree';
+
+const SkillsTree = () => {
+  const data = [];
+  return (
+    <SkillProvider>
+      <SkillTreeGroup>
+        
+      </SkillTreeGroup>
+    </SkillProvider>
+  )
+}
+
+export default SkillsTree
