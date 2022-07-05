483286eca4b596eb9988a20f9183ce2e18efbc7c
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2022-07-05 16:05:00 +0100

    New commit

--- src/containers/SkillsTree/SkillsTree.jsx
+++ src/containers/SkillsTree/SkillsTree.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React from "react";
+import {
+  SkillTreeGroup,
+  SkillTree,
+  SkillProvider,
+  SkillType,
+  SkillGroupDataType,
+} from "beautiful-skill-tree";
 
 // /pathways/:pathwayId/skills-tree
 
 const SkillsTree = () => {
-  
   return (
-    <div>SkillsTree</div>
-  )
-}
+    <div>
+      <p>Hello</p>
+      <SkillProvider>
+        <SkillTreeGroup>
+          {({ skillCount }) => (
+            <SkillTree
+              treeId="first-tree"
+              title="Skill Tree"
+              data={data}
+              collapsible
+              description="My first skill tree"
+            />
+          )}
+        </SkillTreeGroup>
+      </SkillProvider>
+    </div>
+  );
+};
 
-export default SkillsTree
+export default SkillsTree;
