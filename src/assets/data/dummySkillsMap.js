b1f428149f4bb7f138bc835aa88fe9406719894e
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2022-07-06 14:54:17 +0100

    Added unlocked criteria - didnt work

--- src/assets/data/dummySkillsMap.js
+++ src/assets/data/dummySkillsMap.js
@@ -1,68 +1,74 @@
 import { SkillType } from "beautiful-skill-tree";
-import barclays from "../images/barclays.svg"
-import hsbc from "../images/hsbc.svg"
-import goldmanSachs from "../images/goldman-sachs.svg"
-import halifax from "../images/halifax.svg"
-import santander from "../images/santander.svg"
-import play from "../images/play.svg"
+import barclays from "../images/barclays.svg";
+import hsbc from "../images/hsbc.svg";
+import goldmanSachs from "../images/goldman-sachs.svg";
+import halifax from "../images/halifax.svg";
+import santander from "../images/santander.svg";
+import play from "../images/play.svg";
 
 export const data: SkillType = [
-    {
-      id: "play",
-      title: "play",
-      icon: play,
-      tooltip: {
-        content: "Lorem ipsum dolor sit amet. Ut quia recusandae et ipsam sint ea dolores facilis sit nihil quia.",
-      },
-      children: [
-        {
-          id: "barclays",
-          title: "Barclays",
-          icon: barclays,
-          tooltip: {
-            content: "Lorem ipsum dolor sit amet. Ut quia recusandae et ipsam sint ea dolores facilis sit nihil quia.",
-          },
-          children: [
-            {
-              id: "goldman sachs",
-              icon: goldmanSachs,
-              title: "Goldman Sachs",
-              tooltip: {
-                content: "Lorem ipsum dolor sit amet. Ut quia recusandae et ipsam sint ea dolores facilis sit nihil quia.",
-              },
-              children: [],
-            },
-            {
-              id: "hsbc",
-              icon: hsbc,
-              title: "HSBC",
-              tooltip: {
-                content: "Lorem ipsum dolor sit amet. Ut quia recusandae et ipsam sint ea dolores facilis sit nihil quia.",
-              },
-              children: [
-                {
-                  id: "santander",
-                  icon: santander,
-                  title: "Santander",
-                  tooltip: {
-                    content: "Lorem ipsum dolor sit amet. Ut quia recusandae et ipsam sint ea dolores facilis sit nihil quia.",
-                  },
-                  children: [],
-                },
-              ],
-            },
-            {
-              id: "halifax",
-              icon: halifax,
-              title: "Halifax",
-              tooltip: {
-                content: "Lorem ipsum dolor sit amet. Ut quia recusandae et ipsam sint ea dolores facilis sit nihil quia.",
-              },
-              children: [],
-            },
-          ],
-        },
-      ],
+  {
+    id: "play",
+    title: "play",
+    icon: play,
+    tooltip: {
+      content:
+        "Lorem ipsum dolor sit amet. Ut quia recusandae et ipsam sint ea dolores facilis sit nihil quia.",
     },
-  ]
-
+    children: [
+      {
+        id: "barclays",
+        title: "Barclays",
+        icon: barclays,
+        nodeState: "unlocked",
+        tooltip: {
+          content:
+            "Lorem ipsum dolor sit amet. Ut quia recusandae et ipsam sint ea dolores facilis sit nihil quia.",
+        },
+        children: [
+          {
+            id: "goldman sachs",
+            icon: goldmanSachs,
+            title: "Goldman Sachs",
+            tooltip: {
+              content:
+                "Lorem ipsum dolor sit amet. Ut quia recusandae et ipsam sint ea dolores facilis sit nihil quia.",
+            },
+            children: [],
+          },
+          {
+            id: "hsbc",
+            icon: hsbc,
+            title: "HSBC",
+            tooltip: {
+              content:
+                "Lorem ipsum dolor sit amet. Ut quia recusandae et ipsam sint ea dolores facilis sit nihil quia.",
+            },
+            children: [
+              {
+                id: "santander",
+                icon: santander,
+                title: "Santander",
+                tooltip: {
+                  content:
+                    "Lorem ipsum dolor sit amet. Ut quia recusandae et ipsam sint ea dolores facilis sit nihil quia.",
+                },
+                children: [],
+              },
+            ],
+          },
+          {
+            id: "halifax",
+            icon: halifax,
+            title: "Halifax",
+            tooltip: {
+              content:
+                "Lorem ipsum dolor sit amet. Ut quia recusandae et ipsam sint ea dolores facilis sit nihil quia.",
+            },
+            children: [],
+          },
+        ],
+      },
+    ],
+  },
+];
--- src/containers/SkillsTree/SkillsTree.jsx
+++ src/containers/SkillsTree/SkillsTree.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {data} from "./../../assets/data/dummySkillsMap";
+import { data } from "./../../assets/data/dummySkillsMap";
 import {
   SkillTreeGroup,
   SkillTree,
@@ -8,28 +8,20 @@ import {
   SkillGroupDataType,
 } from "beautiful-skill-tree";
 
-
-
 const SkillsTree = (props) => {
   const theme = {
     borderRadius: "50px",
     border: "none",
     treeBackgroundColor: "none",
     nodeBackgroundColor: "white",
-    nodeActiveBackgroundColor: "white"
+    nodeActiveBackgroundColor: "white",
   };
   return (
     <div>
       <SkillProvider>
         <SkillTreeGroup theme={theme}>
           {({ skillCount }) => (
-            <SkillTree
-              treeId="first-tree"
-              title="Skill Tree"
-              data={data}
-              collapsible
-              description="My first skill tree"
-            />
+            <SkillTree treeId="first-tree" title="Skill Tree" data={data} />
           )}
         </SkillTreeGroup>
       </SkillProvider>
