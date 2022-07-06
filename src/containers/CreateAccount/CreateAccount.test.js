e0b4571e08cf778880598af849c080e01138b453
HPTR <76168685+HPTR@users.noreply.github.com>
2022-07-06 16:55:20 +0100

    Test user creation test functional

--- src/containers/CreateAccount/CreateAccount.test.js
+++ src/containers/CreateAccount/CreateAccount.test.js
@@ -1,14 +1,72 @@
 import userEvent from "@testing-library/user-event";
-import CreateAccount from "./CreateAccount"
+import CreateAccount from "./CreateAccount";
+import { render, waitFor, screen } from '@testing-library/react';
+import { customRender } from "../../test-utilities/test-utilities";
+import { type } from "@testing-library/user-event/dist/type";
+
 
 describe("should sign up a new user", () => {
 
-    it("should be possible to save user data", (done) => {
-        const response = fetch("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=1//03WJYY6L8DUPZCgYIARAAGAMSNwF-L9IrXQKV5P1cDzPnYFAoo9RQCitgLA45qNULZjyZ-AOJGZaw93uZ_2lOfl31pQO54KBLLwg", {"email":"test@gmail.com","password":"password"});
-        console.log(response);
-        expect(response).toBeDefined();
-        done();
-    })
+    // it("should be possible to save user data", (done) => {
+    //     const response = fetch("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=1//03WJYY6L8DUPZCgYIARAAGAMSNwF-L9IrXQKV5P1cDzPnYFAoo9RQCitgLA45qNULZjyZ-AOJGZaw93uZ_2lOfl31pQO54KBLLwg", {"email":"test@gmail.com","password":"password"});
+    //     console.log(response);
+    //     expect(response).toBeDefined();
+    //     done();
+    // })
+
+    it("should be possible to save user data", async () => {
+
+        customRender(<CreateAccount />)
+
+        const inputBox = screen.getAllByRole('textbox')[0];
+
+        const response = await waitFor(() => {
+            let headersList = {
+                "Accept": "*/*",
+                "Content-Type": "application/json"
+               }
+               
+               let bodyContent = JSON.stringify({
+                 "email": "test@gmail.com",
+                 "password": "password",
+                 "returnSecureToken": true
+               });
+               
+               return fetch("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDqFVYVthpIozXOTZXcLrt-nSzxJPfaZWY", { 
+                 method: "POST",
+                 body: bodyContent,
+                 headers: headersList
+               }).then(function(response) {
+                 return response.json();
+               }).then(data => data);
+        })
+
+        userEvent.type(inputBox, response.email);
+
+        expect(inputBox).toBeInTheDocument();
+        expect(screen.getByDisplayValue('test@gmail.com')).toBeInTheDocument();
+
+        //Cleaning up by deleting test user for future tests
+
+        await waitFor(() => {
+            let headersList = {
+                "Accept": "*/*",
+                "Content-Type": "application/json"
+               }
+               
+               let bodyContent = JSON.stringify({
+                 "idToken": response.idToken
+               });
+               
+               return fetch("https://identitytoolkit.googleapis.com/v1/accounts:delete?key=AIzaSyDqFVYVthpIozXOTZXcLrt-nSzxJPfaZWY", { 
+                 method: "POST",
+                 body: bodyContent,
+                 headers: headersList
+               });
+        });
+    });
+
+    
 
     // it('checks the function submitTodo', (done) => {
     //     const submitTodo = jest.fn();
