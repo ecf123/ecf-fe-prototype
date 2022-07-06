448052b8eb619616cd643f0ee003953db25e3a15
suarezfer1712 <suarezfer1712@gmail.com>
2022-07-06 17:00:49 +0100

    added new classes

--- src/components/ArticleIndexHeading/ArticleIndexHeading.jsx
+++ src/components/ArticleIndexHeading/ArticleIndexHeading.jsx
@@ -1,32 +1,33 @@
-import React from 'react'
-import {useParams} from "react-router-dom"
-import bookIcon from "../../assets/images/book-icon.svg"
-import "./ArticleIndexHeading.scss"
+import React from "react";
+import { useParams } from "react-router-dom";
+import bookIcon from "../../assets/images/book-icon.svg";
+import "./ArticleIndexHeading.scss";
 
+const ArticleIndexHeading = ({ articleArray }) => {
+  const { articleId } = useParams();
 
-const ArticleIndexHeading = ({articleArray}) => {
-  const {articleId}= useParams();
-
-  const pickedArticle = articleArray.filter(article=>{
-    if(articleId===article.id){
+  const pickedArticle = articleArray.filter((article) => {
+    if (articleId === article.id) {
       return article;
     }
-  })
-  
+  });
 
   return (
-   <article>
-      <h1>{pickedArticle.title}</h1>
-      <div>
-      <p><img src={bookIcon} alt="book icon"/>{pickedArticle.readTime}</p>
-      <p>{pickedArticle.date}</p>
-
+    <article className="article-index-card">
+      <h1 className="article-index-card__title">{pickedArticle.title}</h1>
+      <div className="article-index-card__info">
+        <p>
+          <img
+            className="article-index-card__info__time"
+            src={bookIcon}
+            alt="book icon"
+          />
+          {pickedArticle.readTime}
+        </p>
+        <p className="article-index-card__info__date">{pickedArticle.date}</p>
       </div>
-      
+    </article>
+  );
+};
 
-
-   </article>
-  )
-}
-
-export default ArticleIndexHeading
+export default ArticleIndexHeading;
