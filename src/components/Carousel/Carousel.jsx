92e083e73a5a4f48fe7289b80e2e213981dff7ec
Mark Lawson <mark@walkonmars.co.uk>
2022-07-06 15:08:34 +0100

    completed first test

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -4,7 +4,7 @@ import PathwaysCard from "../PathwaysCard/PathwaysCard";
 
 const Carousel = () => {
   return (
-    <div className="carousel">
+    <div className="carousel" data-testid="carousel">
       <div className="carousel__item">
         <PathwaysCard />
       </div>
--- src/components/Carousel/Carousel.test.js
+++ src/components/Carousel/Carousel.test.js
@@ -1,3 +1,11 @@
 import {screen} from "@testing-library/react";
 import {customRender} from "../../test-utilities/test-utilities";
 import Carousel from "./Carousel";
+
+describe("testing functionality of carousel", () => {
+  it("should render the carousel on the page", () => {
+    customRender(<Carousel />);
+    const carousel = screen.getByTestId("carousel");
+    expect(carousel).toBeInTheDocument();
+  });
+});
