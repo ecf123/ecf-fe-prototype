6bf673ba3077d59b7ab39f3227c5a9d0d239af1b
Gavin Stirling <gavinstir@gmail.com>
2022-07-06 16:31:01 +0100

    Started underline funtion

--- src/components/MenuBar/MenuBar.jsx
+++ src/components/MenuBar/MenuBar.jsx
@@ -2,19 +2,27 @@ import React from "react";
 import "./MenuBar.scss";
 
 const MenuBar = ({ link1, link2, link3, onClickLink }) => {
-  return (
-    <div className="menu-bar">
-      <span className="menu-bar__link" onClick={onClickLink}>
-        {link1}
-      </span>
-      <span className="menu-bar__link" onClick={onClickLink}>
-        {link2}
-      </span>
-      <span className="menu-bar__link" onClick={onClickLink}>
-        {link3}
-      </span>
-    </div>
-  );
+    const [activeArray, setActiveArray] = useState([true, false, false]);
+
+    const underline = (event) => {
+        
+    };
+
+    return (
+        <div className="menu-bar">
+            <span onClick={onClickLink}>
+                <span className="menu-bar__link" onClick={underline}>
+                    {link1}
+                </span>
+            </span>
+            <span className="menu-bar__link" onClick={onClickLink}>
+                {link2}
+            </span>
+            <span className="menu-bar__link" onClick={onClickLink}>
+                {link3}
+            </span>
+        </div>
+    );
 };
 
 export default MenuBar;
