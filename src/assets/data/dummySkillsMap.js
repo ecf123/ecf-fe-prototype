4fc22c2ab9f1c3ebf910799bcca0a488c10cdba5
rachel2bester <rachel2bester@gmail.com>
2022-07-07 18:04:44 +0200

    sorted node glow

--- src/assets/data/dummySkillsMap.js
+++ src/assets/data/dummySkillsMap.js
@@ -4,6 +4,7 @@ import goldmanSachs from "../images/goldman-sachs.svg";
 import halifax from "../images/halifax.svg";
 import santander from "../images/santander.svg";
 import play from "../images/play.svg";
+import podium from "../images/podium.svg";
 
 export const data = [
   {
@@ -11,12 +12,13 @@ export const data = [
     title: "play",
     icon: play,
     locked: false,
-
+    description: "Lorem ipsum dolor sit amet. Ea nisi accusamus 33 Quis quidem ut quia obcaecati ex modi dolore 33 suscipit labore. Ut aliquid molestias aut voluptate incidunt nam assumenda quis et iste vero sit dolor dicta At iusto sunt suscipit nulla.",
     children: [
       {
         id: "barclays",
         title: "Barclays",
         icon: barclays,
+        description: "Lorem ipsum dolor sit amet. Ea nisi accusamus 33 Quis quidem ut quia obcaecati ex modi dolore 33 suscipit labore. Ut aliquid molestias aut voluptate incidunt nam assumenda quis et iste vero sit dolor dicta At iusto sunt suscipit nulla.",
         locked: false,
         parentId: "play",
 
@@ -24,8 +26,8 @@ export const data = [
           {
             id: "goldman sachs",
             icon: goldmanSachs,
+            description: "Lorem ipsum dolor sit amet. Ea nisi accusamus 33 Quis quidem ut quia obcaecati ex modi dolore 33 suscipit labore. Ut aliquid molestias aut voluptate incidunt nam assumenda quis et iste vero sit dolor dicta At iusto sunt suscipit nulla.",
             title: "Goldman Sachs",
-            locked: true,
             parentId: "barclays",
             children: [],
           },
@@ -33,23 +35,34 @@ export const data = [
             id: "hsbc",
             icon: hsbc,
             title: "HSBC",
+            description: "Lorem ipsum dolor sit amet. Ea nisi accusamus 33 Quis quidem ut quia obcaecati ex modi dolore 33 suscipit labore. Ut aliquid molestias aut voluptate incidunt nam assumenda quis et iste vero sit dolor dicta At iusto sunt suscipit nulla.",
             parentId: "barclays",
             children: [
               {
                 id: "santander",
                 icon: santander,
-                nodeState: "unlocked",
+                locked:true,
+                description: "Lorem ipsum dolor sit amet. Ea nisi accusamus 33 Quis quidem ut quia obcaecati ex modi dolore 33 suscipit labore. Ut aliquid molestias aut voluptate incidunt nam assumenda quis et iste vero sit dolor dicta At iusto sunt suscipit nulla.",
                 title: "Santander",
                 parentId: "hsbc",
-                locked: true,
-                children: [],
+                children: [
+                  {
+                    id: "finish",
+                    icon: podium,
+                    locked: true,
+                    description: "Lorem ipsum dolor sit amet. Ea nisi accusamus 33 Quis quidem ut quia obcaecati ex modi dolore 33 suscipit labore. Ut aliquid molestias aut voluptate incidunt nam assumenda quis et iste vero sit dolor dicta At iusto sunt suscipit nulla.",
+                    title: "Santander",
+                    parentId: "santander",
+                    children: []
+                  }
+                ],
               },
             ],
           },
           {
             id: "halifax",
             icon: halifax,
-            nodeState: "unlocked",
+            description: "Lorem ipsum dolor sit amet. Ea nisi accusamus 33 Quis quidem ut quia obcaecati ex modi dolore 33 suscipit labore. Ut aliquid molestias aut voluptate incidunt nam assumenda quis et iste vero sit dolor dicta At iusto sunt suscipit nulla.",
             title: "Halifax",
             parentId: "barclays",
             children: [],
--- src/containers/SkillsTree/SkillsTree.jsx
+++ src/containers/SkillsTree/SkillsTree.jsx
@@ -1,59 +1,84 @@
 import Xarrow from "react-xarrows";
 import SkillsTreeNode from "../../components/SkillsTreeNode/SkillsTreeNode";
 import { data } from "./../../assets/data/dummySkillsMap";
-import React, {useRef} from "react";
-import "./SkillsTree.scss"
+import React, { useRef } from "react";
+import "./SkillsTree.scss";
 
-const SkillsTree = (props) => {
-
+const SkillsTree = () => {
   let levels = [];
   const getNodes = (node, level) => {
-    console.log(node.title + " " + level)
     if (!levels[level]) {
       levels[level] = [];
     }
     levels[level].push(node);
-    node.children.forEach((childNode) => getNodes(childNode, level + 1))
-  }
+    node.children.forEach((childNode) => getNodes(childNode, level + 1));
+  };
 
-  getNodes(data[0], 0)
+  getNodes(data[0], 0);
 
   const getNodeJsx = (node) => {
-    const {icon, title, locked, description, link, id, parentId} = node;
-    console.log(id + " " + parentId);
-    return <div key={id}>
-        <SkillsTreeNode id={id} image={icon} title={title} link={link} locked={locked} description={description}/>
+    const { icon, title, locked, description, link, id, parentId } = node;
+    return (
+      <div key={id}>
+        <SkillsTreeNode
+          id={id}
+          image={icon}
+          title={title}
+          link={link}
+          locked={locked}
+          description={description}
+        />
         {parentId &&
-          (locked
-            ? <Xarrow start={id} end={parentId} startAnchor="top" color="white" strokeWidth={3} showHead={false} path="grid" animateDrawing={false}/>
-            : <Xarrow start={id} end={parentId} startAnchor="top" color="white" strokeWidth={6} showHead={false} path="grid" animateDrawing={false}/>
-          )
-        }
+          (locked ? (
+            <Xarrow
+              start={id}
+              end={parentId}
+              startAnchor="top"
+              endAnchor="middle"
+              color="white"
+              strokeWidth={5}
+              showHead={false}
+              path="grid"
+              gridBreak="100%"
+              animateDrawing={false}
+              SVGcanvasProps={{ strokeOpacity: "50%", }}
+            />
+          ) : (
+              <Xarrow
+                start={id}
+                end={parentId}
+                startAnchor="top"
+                endAnchor="middle"
+                color="white"
+                strokeWidth={6}
+                showHead={false}
+                path="grid"
+                gridBreak="100%"
+                animateDrawing={false}
+              />
+          ))}
       </div>
-    
-  }
+    );
+  };
 
   const getLevelJsx = (level, index) => {
-    let levelJsx = <div className="skills-tree__level" key={"level-" + index}>
-      {level.map((node) => {
-        return getNodeJsx(node)
-        })
-      }
-    </div>
-    return levelJsx
-  }
+    let levelJsx = (
+      <div className="skills-tree__level" key={"level-" + index}>
+        {level.map((node) => {
+          return getNodeJsx(node);
+        })}
+      </div>
+    );
+    return levelJsx;
+  };
 
   const levelsJsx = levels.map((level, index) => {
     return getLevelJsx(level, index);
-  })
+  });
 
   return (
     <div className="skills-tree">
       {levelsJsx}
-      <div id="start">Start Box</div>
-      <p>hi</p>
-      <p>hi</p>
-      <div id="end">End Box</div>
     </div>
   );
 };
