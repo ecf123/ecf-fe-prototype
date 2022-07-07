64bbdcd6e4c9b35c37f5f5c77ad7b58d1d4b0cde
sam <sam_pridmore@hotmail.co.uk>
2022-07-07 15:24:57 +0100

    commit

--- src/containers/Routing/Routing.jsx
+++ src/containers/Routing/Routing.jsx
@@ -15,7 +15,7 @@ import Splash from "../Splash/Splash";
 import CreateAccount from "../CreateAccount/CreateAccount";
 import Articles from "../Articles/Articles";
 import SkillsTree from "../SkillsTree/SkillsTree";
-import Quiz from "../Quiz/Quiz";
+// import Quiz from "../Quiz/Quiz";
 import articleInfo from "../../assets/data/dummyArticleCardInformation";
 import Challenge from "../Challenge/Challenge";
 
