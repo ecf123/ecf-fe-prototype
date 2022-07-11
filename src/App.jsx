53e72ac07a8a4085b829e5e1d96c49cb6f1a5a74
Gavin Stirling <gavinstir@gmail.com>
2022-07-11 16:34:20 +0100

    Completed adaptive styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import "./App.scss";
 import Routing from "./containers/Routing/Routing";
-import FilterTag from './components/FilterTag/FilterTag';
+
 const App = () => {
   return (
     <div className="App">
       <Routing />
-      <FilterTag/>
     </div>
   );
 };
--- src/components/FilterTag/FilterTag.jsx
+++ src/components/FilterTag/FilterTag.jsx
@@ -1,26 +1,199 @@
-import React from 'react'
-import "./FilterTag.scss"
-import performanceMoney from "../../assets/images/performance-money-icon.svg"
-import handshake from "../../assets/images/handshake-icon.svg"
-import paintingPalette from "../../assets/images/painting-palette-icon.svg"
-import phoneWrite from "../../assets/images/phone-write-icon.svg"
-import carActions from "../../assets/images/car-actions-icon.svg"
-import fileCode from "../../assets/images/file-code-icon.svg"
-
+import React, { useState } from "react";
+import "./FilterTag.scss";
+import performanceMoney from "../../assets/images/performance-money-icon.svg";
+import handshake from "../../assets/images/handshake-icon.svg";
+import paintingPalette from "../../assets/images/painting-palette-icon.svg";
+import phoneWrite from "../../assets/images/phone-write-icon.svg";
+import carActions from "../../assets/images/car-actions-icon.svg";
+import fileCode from "../../assets/images/file-code-icon.svg";
 
 const FilterTag = () => {
-  return (
-    <div className='filter-tag'> trial
-      <span className='filter-tag__item'>All</span>
-      <span className='filter-tag__item'>finance</span>
-      <span className='filter-tag__item'>Business</span>
-      <span className='filter-tag__item'>Design</span>
-      <span className='filter-tag__item'>Digital Marketing</span>
-      <span className='filter-tag__item'>Engineering</span>
-      <span className='filter-tag__item'>Programming</span>
+    const [activeArray, setActiveArray] = useState([
+        true,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+    ]);
 
-    </div>
-  )
-}
+    const handleSelected = (event) => {
+        setActiveArray([false, false, false, false, false, false, false]);
+        switch (event.target.id) {
+            case "all":
+                setActiveArray([
+                    false,
+                    true,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                ]);
+                break;
+            case "1":
+                setActiveArray([
+                    false,
+                    true,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                ]);
+                break;
+            case "2":
+                setActiveArray([
+                    false,
+                    false,
+                    true,
+                    false,
+                    false,
+                    false,
+                    false,
+                ]);
+                break;
+            case "3":
+                setActiveArray([
+                    false,
+                    false,
+                    false,
+                    true,
+                    false,
+                    false,
+                    false,
+                ]);
+                break;
+            case "4":
+                setActiveArray([
+                    false,
+                    false,
+                    false,
+                    false,
+                    true,
+                    false,
+                    false,
+                ]);
+                break;
+            case "5":
+                setActiveArray([
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    true,
+                    false,
+                ]);
+                break;
+            case "6":
+                setActiveArray([
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    true,
+                ]);
+                break;
+            default:
+                setActiveArray([
+                    true,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                ]);
+                break;
+        }
+    };
 
-export default FilterTag
+    return (
+        <div className="filter-tag">
+            <span
+                id="all"
+                className={
+                    activeArray[0]
+                        ? "filter-tag__item filter-tag__active"
+                        : "filter-tag__item"
+                }
+                onClick={handleSelected}
+            >
+                {"    "}
+                All{"   "}
+            </span>
+            <span
+                id="1"
+                className={
+                    activeArray[1]
+                        ? "filter-tag__item filter-tag__active"
+                        : "filter-tag__item"
+                }
+                onClick={handleSelected}
+            >
+                <img src={performanceMoney} alt="" /> Finance
+            </span>
+            <span
+                id="2"
+                className={
+                    activeArray[2]
+                        ? "filter-tag__item filter-tag__active"
+                        : "filter-tag__item"
+                }
+                onClick={handleSelected}
+            >
+                <img src={handshake} alt="" /> Business
+            </span>
+            <span
+                id="3"
+                className={
+                    activeArray[3]
+                        ? "filter-tag__item filter-tag__active"
+                        : "filter-tag__item"
+                }
+                onClick={handleSelected}
+            >
+                <img src={paintingPalette} alt="" /> Design
+            </span>
+            <span
+                id="4"
+                className={
+                    activeArray[4]
+                        ? "filter-tag__item filter-tag__active"
+                        : "filter-tag__item"
+                }
+                onClick={handleSelected}
+            >
+                <img src={phoneWrite} alt="" /> Digital Marketing
+            </span>
+            <span
+                id="5"
+                className={
+                    activeArray[5]
+                        ? "filter-tag__item filter-tag__active"
+                        : "filter-tag__item"
+                }
+                onClick={handleSelected}
+            >
+                <img src={carActions} alt="" /> Engineering
+            </span>
+            <span
+                id="6"
+                className={
+                    activeArray[6]
+                        ? "filter-tag__item filter-tag__active"
+                        : "filter-tag__item"
+                }
+                onClick={handleSelected}
+            >
+                <img src={fileCode} alt="" /> Programming
+            </span>
+        </div>
+    );
+};
+
+export default FilterTag;
