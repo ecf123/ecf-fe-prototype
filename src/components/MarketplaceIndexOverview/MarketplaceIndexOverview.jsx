e9eb3035488d170e8a13aae432cb3f4edcfe32d8
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2022-07-12 15:33:38 +0100

    Updated MarketplaceIndexOverview and basic styling

--- src/components/MarketplaceIndexOverview/MarketplaceIndexOverview.jsx
+++ src/components/MarketplaceIndexOverview/MarketplaceIndexOverview.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import "./MarketplaceIndexOverview.scss";
 
 const MarketplaceIndexOverview = ({ marketData }) => {
   const { marketplaceId } = useParams();
   console.log(marketData);
   console.log(marketplaceId);
-  const pickedMarketCard = marketData.find(({ id }) => id === marketplaceId);
+  const pickedMarketCard = marketData.find(
+    ({ id }) => id === parseInt(marketplaceId)
+  );
 
-  return <div>{pickedMarketCard.id}</div>;
+  return (
+    <div className="marketplace-overview-container">
+      <h1 className="marketplace-overview-container__title">Overview</h1>
+      <p className="marketplace-overview-container__info">
+        {pickedMarketCard.overview}
+      </p>
+    </div>
+  );
 };
 
 export default MarketplaceIndexOverview;
