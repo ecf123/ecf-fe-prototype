5cf7d6bc26c89fc04d2aa3af3d3371c822aaa673
HPTR <76168685+HPTR@users.noreply.github.com>
2022-07-12 16:36:50 +0100

    Update and add tests for recent changes

--- src/components/Carousel/Carousel.test.js
+++ src/components/Carousel/Carousel.test.js
@@ -4,15 +4,22 @@ import Carousel from "./Carousel";
 
 describe("testing functionality of carousel", () => {
   it("should render the carousel on the page", () => {
-    customRender(<Carousel />);
+    customRender(<Carousel cardData={[1,2,3]} />);
     const carousel = screen.getByTestId("carousel");
     expect(carousel).toBeInTheDocument();
   });
 
-  // testing the item appears
   it("should render the carousel item on the page", () => {
-    customRender(<Carousel />);
+    customRender(<Carousel cardData={[1]} />);
     const carouselItem = screen.getByTestId("carousel__item");
     expect(carouselItem).toBeInTheDocument();
+  });
+
+  it("should render text based on props", () => {
+    customRender(<Carousel cardData={[{header: "Financial Forest", topics: "Banking, Investments", image: null}]} />);
+    const financialForestText = screen.getByText("Financial Forest");
+    const topicsText = screen.getByText("Banking, Investments");
+    expect(financialForestText).toBeInTheDocument();
+    expect(topicsText).toBeInTheDocument();
   });
 });
