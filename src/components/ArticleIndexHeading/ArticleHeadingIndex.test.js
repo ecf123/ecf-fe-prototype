ee909eb1866193b3061d7aeff53c5fd3df4ea4f4
Younus Khan <younuskhan99@hotmail.com>
2022-07-12 15:05:07 +0100

    bug fixes

--- src/components/ArticleIndexHeading/ArticleHeadingIndex.test.js
+++ src/components/ArticleIndexHeading/ArticleHeadingIndex.test.js
@@ -5,20 +5,20 @@ import articleData from "../../assets/data/dummyArticles";
 
 jest.mock("react-router-dom", () => ({
     ...jest.requireActual("react-router-dom"),
-    useParams: () => ({ articleId: 1 }),
+    useParams: () => ({ articleId: 1 })
 }));
-  
+
 describe("initial tests for Article heading index", () => {
     it("should render the heading from the first article in dummy data", () => {
         customRender(<ArticleHeaderIndex articleArray={articleData} />);
-        const title = screen.getByTestId("article-heading");
+        const title = screen.getByTestId("heading");
         expect(title).toBeInTheDocument();
     });
 
     it("should render the date and read time from the first article in dummy data", () => {
         customRender(<ArticleHeaderIndex articleArray={articleData} />);
-        const date = screen.getByTestId("article-date");
-        const time = screen.getByTestId("article-time");
+        const date = screen.getByTestId("date");
+        const time = screen.getByTestId("time");
 
         expect(date).toBeInTheDocument();
         expect(time).toBeInTheDocument();
--- src/components/ArticleIndexHeading/ArticleIndexHeading.jsx
+++ src/components/ArticleIndexHeading/ArticleIndexHeading.jsx
@@ -5,16 +5,18 @@ import "./ArticleIndexHeading.scss";
 
 const ArticleIndexHeading = ({ articleArray }) => {
   const { articleId } = useParams();
+  console.log(articleId);
 
-  const pickedArticle = articleArray.find( ({ id }) => id.toString() === articleId)
+  const pickedArticle = articleArray.find( ({ id }) => id === articleId)
+  console.log(pickedArticle);
 
   return (
     <article className="article-index-card">
-      <h1 data-testid="article-heading" className="article-index-card__title">{pickedArticle.title}</h1>
+      <h1 data-testid="heading" className="article-index-card__title">{pickedArticle.title}</h1>
       <div className="article-index-card__info">
         <img className="article-index-card__info__img" src={bookIcon} alt="book icon"/>
-        <p data-testid="article-time" className="article-index-card__info__time">{pickedArticle.readTime}</p>
-        <p data-testid="article-date" className="article-index-card__info__date">{pickedArticle.date}</p>
+        <p data-testid="time" className="article-index-card__info__time">{pickedArticle.readTime}</p>
+        <p data-testid="date" className="article-index-card__info__date">{pickedArticle.date}</p>
       </div>
     </article>
   );
