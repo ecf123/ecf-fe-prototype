0c0f5e9acbb1802ffe1f3b730509236d5121aa9e
Hassan <hassan.seedat07@gmail.com>
2022-07-13 15:30:12 +0100

    added navigation bar, articleindex heading content

--- src/assets/data/dummyArticles.js
+++ src/assets/data/dummyArticles.js
@@ -1,22 +1,41 @@
+const articleContent = [
+	{
+	heading: "Heading 1",
+	paragraph: "Lorem ipsum..."
+	},
+	{
+	heading: "Heading 2",
+	paragraph: "Lorem ipsum..."
+	}
+]
+
 const articleData = [
     {
-        title: "Want a winning CV? Folow these 6 tips",
+        title: "Want a winning CV? Follow these 6 tips",
+        articleContent: articleContent,
         readTime: "5 mins",
         date: "23 May 2022",
         id: 1
     },
     {
-        title: "Want a winning CV? Folow these 7 tips",
+        title: "Want a winning CV? Follow these 7 tips",
+        articleContent: articleContent,
         readTime: "5 mins",
         date: "23 May 2022",
         id: 2
     },
     {
-        title: "Want a winning CV? Folow these 8 tips",
+        title: "Want a winning CV? Follow these 8 tips",
+        articleContent: articleContent,
         readTime: "5 mins",
         date: "23 May 2022",
         id: 3
     }
 ];
+
+
+
+
+
 
 export default articleData;
--- src/containers/ArticleIndex/ArticleIndex.jsx
+++ src/containers/ArticleIndex/ArticleIndex.jsx
@@ -1,14 +1,35 @@
 import React from 'react'
-import ArticleIndexHeading from '../../components/ArticleIndexHeading/ArticleIndexHeading'
-
+import ArticleIndexHeading from '../../components/ArticleIndexHeading/ArticleIndexHeading';
+import Navigation from '../../components/Navigation/Navigation';
+import { useParams } from 'react-router-dom';
 
 // path: /articles/:articleId
 
+
 const ArticleIndex = ({articleArray}) => {
+  const { articleId } = useParams();
+
+  const pickedArticle = articleArray.find( ({ id }) => id === parseInt(articleId))
+
+
+
   return (
-    <div>
+    <div className='articleIndex'>
       <ArticleIndexHeading articleArray={articleArray}/>
+      <div className='articleIndex__container'>
+      {pickedArticle.articleContent.map((item,index) => {
+        return(
+          <div className ='articleIndex__content' key={index}>
+          <h2 className='articleIndex__heading'>{item.heading}</h2>
+          <p className='articleIndex__paragraph'>{item.paragraph}</p>
+          </div>  )
+        
+      })}
+      
+      </div> 
+      <Navigation />
     </div>
+
   )
 }
 
