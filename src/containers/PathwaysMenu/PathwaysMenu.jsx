5707ea725137e7448bde2371ddcacd947aedd501
Gavin Stirling <gavinstir@gmail.com>
2022-07-13 14:45:13 +0100

    Pulled all required components in

--- src/containers/PathwaysMenu/PathwaysMenu.jsx
+++ src/containers/PathwaysMenu/PathwaysMenu.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import TrophyHeader from "../../components/TrophyHeader/TrophyHeader.jsx";
 import FilterTag from "../../components/FilterTag/FilterTag.jsx";
 import PathwayCardList from "../PathwayCardList/PathwayCardList";
+import Navigation from "../../components/Navigation/Navigation.jsx";
+
+// Link to dummy data
+import userProfile from "../../assets/data/dummyUserData.js";
 
 // path: /pathways
 
@@ -10,10 +15,19 @@ const PathwaysMenu = () => {
     };
 
     return (
-        <div>
-            <div>
+        <div className="pathways-menu">
+            <div className="pathways-menu__header">
+                <TrophyHeader userProfile={userProfile} />
+            </div>
+            <h1 className="pathways-menu__heading">Pathways</h1>
+            <div className="pathways-menu__filters">
                 <FilterTag filterArray={filterArray} />
+            </div>
+            <div className="pathways-menu__list">
                 <PathwayCardList />
+            </div>
+            <div className="pathways-menu__navbar">
+                <Navigation />
             </div>
         </div>
     );
