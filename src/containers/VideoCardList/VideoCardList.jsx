38a3bdda9e9f06c7ef39c7ce00c2f9667364e5d7
Mark Lawson <mark@walkonmars.co.uk>
2022-07-13 16:29:42 +0100

    video card list tested for icon rendering

--- src/containers/VideoCardList/VideoCardList.jsx
+++ src/containers/VideoCardList/VideoCardList.jsx
@@ -2,23 +2,13 @@ import React from "react";
 import "./VideoCardList.scss";
 import VideoCard from "../../components/VideoCard/VideoCard";
 
-const VideoCardList = ({ dummyVideoData, id }) => {
+const VideoCardList = ({dummyVideoData, id}) => {
   return (
     <>
       <div className="video-card-list">
         <VideoCard dummyVideoData={dummyVideoData} id={id} />
-        <img
-          className="video-card-list__icon"
-          src={dummyVideoData[id].icon}
-          alt={`Icon for ${dummyVideoData[id].name}`}
-        />
+        <img className="video-card-list__icon" src={dummyVideoData[id].icon} alt={`Icon for ${dummyVideoData[id].name}`} />
       </div>
-      {/* {dummyVideoData.map(({url, icon, name}, index) => (
-        <div className="video-card-list" key={index}>
-          <VideoCard url={url} />
-          <img className="video-card-list__icon" src={icon} alt={`Icon for ${name}`} />
-        </div>
-      ))} */}
     </>
   );
 };
--- src/containers/VideoCardList/VideoCardList.test.js
+++ src/containers/VideoCardList/VideoCardList.test.js
@@ -1,16 +1,13 @@
-import { render, screen, waitFor } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import {screen} from "@testing-library/react";
+import {customRender} from "../../test-utilities/test-utilities";
 import dummyVideoData from "../../assets/data/dummyVideoCardData";
 import VideoCardList from "./VideoCardList";
-import { act } from "react-dom/test-utils";
-import { customRender } from "../../test-utilities/test-utilities";
-
 
 describe("basic tests for card  list component", () => {
-  it("Should render the image on card list", () => {
+  it("Should render the icon image on card list", () => {
     customRender(<VideoCardList dummyVideoData={dummyVideoData} id={1} />);
-    const img = screen.getByRole("img");
-    expect(img).toBeInTheDocument();
-    expect(img).not.toBeNull();
+    const icon = screen.getByRole("img");
+    expect(icon).toBeInTheDocument();
+    expect(icon).not.toBeNull();
   });
 });
