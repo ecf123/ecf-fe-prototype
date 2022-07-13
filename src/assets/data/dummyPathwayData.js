d0704058a3b7814652d0440e9d0ddfdcc6fed297
carapaceonbear <seerhan17@gmail.com>
2022-07-13 19:17:36 +0100

    home page improvements

--- src/assets/data/dummyPathwayData.js
+++ src/assets/data/dummyPathwayData.js
@@ -6,12 +6,12 @@ import codingCamp from "../images/coding-camp.svg";
 import epicEngineering from "../images/epic-engineering.svg";
 
 //Percentage data will eventually have to be stored in userData as users progression will vary -- see dummy user data for example
-const cardData = [{header: "Financial Forest", topics: "Banking, Investments", image: financialForest, percentage: 90},
-{header: "Business Bonanza", topics: "Business, Strategy", image: businessBonanza, percentage: 35},
-{header: "Divine Design", topics: "UI/UX, Motion, Graphic Design", image: divineDesign, percentage: 1},
-{header: "Magic Marketing", topics: "Digital Marketing, Copywriting", image: magicMarketing, percentage: 5},
-{header: "Coding Camp", topics: "Software, Web Development", image: codingCamp, percentage: 0},
-{header: "Epic Engineering", topics: "Civil, Aerospace Engineering", image: epicEngineering, percentage: 0}];
+const cardData = [{id: "card1", header: "Financial Forest", topics: "Banking, Investments", image: financialForest, percentage: 90},
+{id: "card2", header: "Business Bonanza", topics: "Business, Strategy", image: businessBonanza, percentage: 35},
+{id: "card3",header: "Divine Design", topics: "UI/UX, Motion, Graphic Design", image: divineDesign, percentage: 1},
+{id: "card4",header: "Magic Marketing", topics: "Digital Marketing, Copywriting", image: magicMarketing, percentage: 5},
+{id: "card5",header: "Coding Camp", topics: "Software, Web Development", image: codingCamp, percentage: 0},
+{id: "card6",header: "Epic Engineering", topics: "Civil, Aerospace Engineering", image: epicEngineering, percentage: 0}];
 
 
 export default cardData;
