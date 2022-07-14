efb87a7b5b8cf592e261b2ef1139151c7798212f
HPTR <76168685+HPTR@users.noreply.github.com>
2022-07-14 18:23:45 +0100

    Reformat ArticleIndex

--- src/containers/ArticleIndex/ArticleIndex.jsx
+++ src/containers/ArticleIndex/ArticleIndex.jsx
@@ -1,16 +1,17 @@
-import {React, useState} from 'react'
+import { React, useState } from 'react'
 import ArticleIndexHeading from '../../components/ArticleIndexHeading/ArticleIndexHeading';
 import Navigation from '../../components/Navigation/Navigation';
 import { useParams } from 'react-router-dom';
 import BackButton from '../../components/BackButton/BackButton';
 import SaveButton from '../../components/SaveButton/SaveButton';
+import "./ArticleIndex.scss";
 // path: /articles/:articleId
 
 
-const ArticleIndex = ({articleArray}) => {
+const ArticleIndex = ({ articleArray }) => {
   const { articleId } = useParams();
 
-  const pickedArticle = articleArray.find( ({ id }) => id === parseInt(articleId))
+  const pickedArticle = articleArray.find(({ id }) => id === parseInt(articleId))
 
 
 
@@ -22,19 +23,22 @@ const ArticleIndex = ({articleArray}) => {
 
   return (
     <div className='articleIndex'>
-      <div className='articleIndex__header' ><BackButton linkTo="/articles"/><SaveButton handleSaveButtonPress={onSaveButtonPress} isPressed={isPressed}/> </div>
-      <ArticleIndexHeading articleArray={articleArray}/>
+      <div className='articleIndex__header' >
+        <BackButton linkTo="/articles" />
+        <SaveButton handleSaveButtonPress={onSaveButtonPress} isPressed={isPressed} />
+      </div>
+      <ArticleIndexHeading articleArray={articleArray} />
       <div className='articleIndex__container'>
-      {pickedArticle.articleContent.map((item,index) => {
-        return(
-          <div className ='articleIndex__content' key={index}>
-          <h2 className='articleIndex__heading'>{item.heading}</h2>
-          <p className='articleIndex__paragraph'>{item.paragraph}</p>
-          </div>  )
-        
-      })}
-      
-      </div> 
+        {pickedArticle.articleContent.map((item, index) => {
+          return (
+            <div className='articleIndex__content' key={index}>
+              <h2 className='articleIndex__heading'>{item.heading}</h2>
+              <p className='articleIndex__paragraph'>{item.paragraph}</p>
+            </div>)
+
+        })}
+
+      </div>
       <Navigation />
     </div>
 
