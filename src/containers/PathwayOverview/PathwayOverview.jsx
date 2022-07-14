cd418545ac91c120d9abf363efe20a985da1c4ee
sam <sam_pridmore@hotmail.co.uk>
2022-07-14 14:23:04 +0100

    added testing

--- src/containers/PathwayOverview/PathwayOverview.jsx
+++ src/containers/PathwayOverview/PathwayOverview.jsx
@@ -8,32 +8,40 @@ import MenuBar from '../../components/MenuBar/MenuBar';
 // path: /pathways/:pathwayId
 
 const PathwayOverview = ({ userProfile, card }) => {
-  const { header, topics, image, overview, structure, careers } = card;
-
   const [category, setCategory] = useState('overview');
 
-  const overviewJSX = overview.split('.').map((sentence) => {
-    return <p>{sentence}</p>;
-  });
-  const structureJSX = structure.split('.').map((sentence) => {
-    return <p>{sentence}</p>;
-  });
-  const careersJSX = careers.split('.').map((sentence) => {
-    return <p>{sentence}</p>;
-  });
+  // PROPS TO USE WHEN PATHWAY CARD LIST IS LINKED AND CAN GIVE AN ID
+  // const { header, topics, image, id, overview, structure, careers } = card;
+
+  
+  const cardOne = card[0];
+  
+  // --------------- (id.overview)
+  const overviewJSX = cardOne.overview.split('.').map((sentence, index) => {
+      return <p key={index}>{sentence}</p>;
+    });
+
+  const structureJSX = cardOne.structure.split('.').map((sentence, index) => {
+      return <p key={index}>{sentence}</p>;
+    });
+
+  const careersJSX = cardOne.careers.split('.').map((sentence, index) => {
+      return <p key={index}>{sentence}</p>;
+    });
+
+
   const handleCategoryChange = (event) => {
     setCategory(event.target.innerText.toLowerCase());
-    console.log(event.target.innerText.toLowerCase());
   };
 
   const changeCategory = () => {
     switch (category) {
       case 'overview':
         return overviewJSX;
-      case 'structure':
-        return structureJSX;
-      case 'careers':
-        return careersJSX;
+        case 'structure':
+          return structureJSX;
+        case 'careers':
+          return careersJSX;
       default:
         console.log('not working');
         return;
@@ -43,16 +51,16 @@ const PathwayOverview = ({ userProfile, card }) => {
   return (
     <div className="pathway-overview">
       <header className="pathway-overview__header">
-      <div className='pathway-overview__backButton'>
-        <BackButton linkTo="linkTo" />
+        <div className="pathway-overview__backButton">
+          <BackButton linkTo="linkTo" />
         </div>
-        <div className='pathway-overview__trophy'>
-        <TrophyStats userProfile={userProfile} />
+        <div className="pathway-overview__trophy">
+          <TrophyStats userProfile={userProfile} />
         </div>
       </header>
       <div className="card-container">
-        <h1 className="card-container__header">{header}</h1>
-        <img className="card-container__image" src={image} alt={topics} />
+        <h1 className="card-container__header">{cardOne.header}</h1>
+        <img className="card-container__image" data-testid="main-image" src={cardOne.image} alt={cardOne.topics} />
       </div>
       <div className="pathway-overview__filters">
         <MenuBar
@@ -62,7 +70,7 @@ const PathwayOverview = ({ userProfile, card }) => {
           onClickLink={handleCategoryChange}
         />
       </div>
-      <div className="pathway-overview__content">{changeCategory()}</div>
+      <div className="pathway-overview__content" data-testid="main-content">{changeCategory()}</div>
 
       <div className="pathway-overview__button">
         <Button buttonText={'START PATHWAYS'} />
