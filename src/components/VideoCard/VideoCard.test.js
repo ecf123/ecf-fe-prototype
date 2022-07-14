7708d9bdaef1932e8e1a16a7d2a3b7836c665037
Mark Lawson <mark@walkonmars.co.uk>
2022-07-14 16:45:11 +0100

    removed whitespace in test file

--- src/components/VideoCard/VideoCard.test.js
+++ src/components/VideoCard/VideoCard.test.js
@@ -1,18 +1,12 @@
-import {render, screen, waitFor} from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import {screen} from "@testing-library/react";
 import dummyVideoData from "../../assets/data/dummyVideoCardData";
 import VideoCard from "./VideoCard";
-import { act } from "react-dom/test-utils";
-import { wait } from "@testing-library/user-event/dist/utils";
-import { customRender } from "../../test-utilities/test-utilities";
+import {customRender} from "../../test-utilities/test-utilities";
 
 describe("basic tests for card component", () => {
-it("Should render the card list", () => {
+  it("Should render the card list", () => {
     customRender(<VideoCard dummyVideoData={dummyVideoData} id={1} />);
-    
     const videoCard = screen.getByTestId("video-card");
-    
-    
     expect(videoCard).toBeInTheDocument();
   });
-})
+});
