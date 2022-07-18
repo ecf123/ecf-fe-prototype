98eca4e3e9889be10141bee560d1914e6bae9a85
aiman.wahizam <aiman.wahizam@nology.io>
2022-07-18 16:26:06 +0100

    added some styling to the page. fix button

--- src/containers/MultiplChoiceEndScreen/MultipleChoiceEndScreen.jsx
+++ src/containers/MultiplChoiceEndScreen/MultipleChoiceEndScreen.jsx
@@ -1,25 +1,35 @@
 import React from "react";
-import "./MultipleChoiceEndScreen.scss"
+import "./MultipleChoiceEndScreen.scss";
 import podium from "../../assets/images/podium.svg";
 import line from "../../assets/images/line.svg";
 import Button from "../../components/Button/Button";
+import retakeIcon from "../../assets/images/redo.svg";
 
 //challenge/multiple-choice-end-screen
 
-const MultipleChoiceEndScreen = ({score}) => {
+const MultipleChoiceEndScreen = ({ score }) => {
   return (
     <div className="end-screen">
-   <h1 className="end-screen__title">Multiple Choice Quiz</h1>
-   <div>
-   <img className="end-screen__podium" src={podium} alt="Congratulations" />
-   <img className="end-screen__underline" src={line} alt="underline" />
-   </div>
-   <h2 className="end-screen__message">Congratulations!! You passed with a score of {score} </h2>
-   <div>
-   <Button className="end-screen__re-take-button"  buttonText="RE-TAKE"/>
-   <Button className="end-screen__continue-button" isSecondary={true} buttonText="CONTINUE"/>
-   </div>
-   </div>
+      <h1 className="end-screen__title">Multiple Choice Quiz</h1>
+      <div>
+        <img
+          className="end-screen__podium"
+          src={podium}
+          alt="Congratulations"
+        />
+        <img className="end-screen__underline" src={line} alt="underline" />
+      </div>
+      <h2 className="end-screen__message">
+        Congratulations!! You passed with a score of {score}{" "}
+      </h2>
+      <div className="end-screen__button">
+        <Button buttonText="RE-TAKE" />
+        <Button
+          isSecondary={true}
+          buttonText="CONTINUE"
+        />
+      </div>
+    </div>
   );
 };
 
