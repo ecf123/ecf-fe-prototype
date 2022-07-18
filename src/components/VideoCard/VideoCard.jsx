8744f76f153593b679f117269097f751df965d39
carapaceonbear <seerhan17@gmail.com>
2022-07-18 15:41:13 +0100

    component taking data

--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -5,10 +5,10 @@ import ReactPlayer from "react-player";
 // LINK TO DOCS FOR VIDEO PLAYER
 // https://openbase.com/js/react-player/documentation
 
-const VideoCard = ({dummyVideoData, id}) => {
+const VideoCard = ({videoData}) => {
   return (
     <div data-testid="video-card" className="video-card">
-      {process.env.NODE_ENV !== "test" && <ReactPlayer url={dummyVideoData[id].url} width="100%" height="242px" />}
+      {process.env.NODE_ENV !== "test" && <ReactPlayer url={videoData.url} width="100%" height="242px" />}
     </div>
   );
 };
--- src/containers/LessonOverview/LessonOverview.jsx
+++ src/containers/LessonOverview/LessonOverview.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import VideoCard from '../../components/VideoCard/VideoCard';
 import BackButton from '../../components/BackButton/BackButton';
 import TrophyStats from '../../components/TrophyStats/TrophyStats';
-import data from "../../assets/data/dummyVideoCardData.js"
+import data from "../../assets/data/dummyLessonOverview.js"
 
 import "./LessonOverview.scss";
 
 // path: /lesson/:lessonId
+
+const lessonData = data[0];
+
+const bulletPoints = lessonData.bullets.split("*").map(sentence => <li>{sentence}</li>);
 
 const LessonOverview = ({userProfile}) => {
   return (
@@ -21,13 +25,17 @@ const LessonOverview = ({userProfile}) => {
       </header>
       <div className="contents">
         <div className="contents__video">
-          <VideoCard className="contents__video--temp" dummyVideoData={data} id={1} />
+          <VideoCard className="contents__video--temp" videoData={lessonData.video} />
         </div>
-        <h1 className="contents__title">The Role of etc.</h1>
+        <h1 className="contents__title">{lessonData.title}</h1>
         <h2 className="contents__header">Overview</h2>
-        <p className="contents__paragraph">lorem</p>
-        <h2 className="contents__header">Introduction</h2>
-        <p className="contents__paragraph">lorem</p>
+        <p className="contents__paragraph">
+          {bulletPoints}
+        </p>
+        <h2 className="contents__header">{lessonData.headingOne}</h2>
+        <p className="contents__paragraph">{lessonData.paragraphOne}</p>
+        <h2 className="contents__header">{lessonData.headingTwo}</h2>
+        <p className="contents__paragraph">{lessonData.paragraphTwo}</p>
       </div>
     </div>
   )
