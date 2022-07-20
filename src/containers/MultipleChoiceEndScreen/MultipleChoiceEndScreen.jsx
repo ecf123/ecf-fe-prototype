2c6eb7fd80b91f9d367fb18ea87a18c6b1dc5fde
aiman.wahizam <aiman.wahizam@nology.io>
2022-07-20 15:18:01 +0100

    finished rendering now test

--- src/containers/MultipleChoiceEndScreen/MultipleChoiceEndScreen.jsx
+++ src/containers/MultipleChoiceEndScreen/MultipleChoiceEndScreen.jsx
@@ -4,10 +4,11 @@ import podium from "../../assets/images/podium.svg";
 import line from "../../assets/images/line.svg";
 import Button from "../../components/Button/Button";
 import retakeIcon from "../../assets/images/retake.svg";
+import { Link } from "react-router-dom";
 
 //challenge/multiple-choice-end-screen
 
-const MultipleChoiceEndScreen = ({ score }) => {
+const MultipleChoiceEndScreen = ({ score, quizLink, courseOverviewLink }) => {
   return (
     <div className="end-screen">
       <h1 className="end-screen__title">Multiple Choice Quiz</h1>
@@ -20,16 +21,22 @@ const MultipleChoiceEndScreen = ({ score }) => {
         <img className="end-screen__underline" src={line} alt="underline" />
       </div>
       <h2 className="end-screen__message">
-        Congratulations!! You passed with a score of {score}{" "}
+        Congratulations!! You passed with a score of {score}
       </h2>
       <div className="end-screen__button">
-        <button
-          className="end-screen__button--retake"
-        >
-          <img src={retakeIcon}  alt="re-take" className="end-screen__retake-icon"/>
-          RE-TAKE
-        </button>
-        <Button isSecondary={true} buttonText="CONTINUE" />
+        {/* <Link to={quizLink}> */}
+          <button className="end-screen__button--retake">
+            <img
+              src={retakeIcon}
+              alt="re-take"
+              className="end-screen__retake-icon"
+            />
+            RE-TAKE
+          </button>
+        {/* </Link> */}
+        {/* <Link to={courseOverviewLink}> */}
+          <Button isSecondary={true} buttonText="CONTINUE" />
+        {/* </Link> */}
       </div>
     </div>
   );
