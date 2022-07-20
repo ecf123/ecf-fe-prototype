abc659cbd0be5586f0febc8c1c15bcbff01a3b33
awo-ali <awoali@hotmail.co.uk>
2022-07-20 16:03:28 +0100

    MultipleChoiceEndScreen changes

--- src/containers/MultipleChoiceEndScreen/MultipleChoiceEndScreen.jsx
+++ src/containers/MultipleChoiceEndScreen/MultipleChoiceEndScreen.jsx
@@ -11,31 +11,41 @@ import { Link } from "react-router-dom";
 const MultipleChoiceEndScreen = ({ score, quizLink, courseOverviewLink }) => {
   return (
     <div className="end-screen">
-      <h1 className="end-screen__title">Multiple Choice Quiz</h1>
+      <h1 className="end-screen__title" data-testid="end-screen-title">
+        Multiple Choice Quiz
+      </h1>
       <div>
         <img
           className="end-screen__podium"
           src={podium}
           alt="Congratulations"
+          data-testid="end-screen-podium"
         />
         <img className="end-screen__underline" src={line} alt="underline" />
       </div>
-      <h2 className="end-screen__message">
+      <h2 className="end-screen__message" data-testid="end-screen-message">
         Congratulations!! You passed with a score of {score}
       </h2>
       <div className="end-screen__button">
         {/* <Link to={quizLink}> */}
-          <button className="end-screen__button--retake">
-            <img
-              src={retakeIcon}
-              alt="re-take"
-              className="end-screen__retake-icon"
-            />
-            RE-TAKE
-          </button>
+        <button
+          className="end-screen__button--retake"
+          data-testid="end-screen-button-retake"
+        >
+          <img
+            src={retakeIcon}
+            alt="re-take"
+            className="end-screen__retake-icon"
+          />
+          RE-TAKE
+        </button>
         {/* </Link> */}
         {/* <Link to={courseOverviewLink}> */}
-          <Button isSecondary={true} buttonText="CONTINUE" />
+        <Button
+          isSecondary={true}
+          buttonText="CONTINUE"
+          data-testid="end-screen-button-continue"
+        />
         {/* </Link> */}
       </div>
     </div>
