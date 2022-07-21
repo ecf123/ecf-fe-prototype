00826861936f6bb4bde2dfb63b99559b4adb2712
Charlie Richardson <55165280+Charlie-robin@users.noreply.github.com>
2022-07-21 17:08:09 +0100

    Revert "61 feature authentication private routing after sign in"

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,10 @@
-import { onAuthStateChanged } from "firebase/auth";
-import { useEffect, useState } from "react";
 import "./App.scss";
 import Routing from "./containers/Routing/Routing";
-import { auth } from "./firebase";
-
 
 const App = () => {
-  const [userToken, setUserToken] = useState();
-  useEffect(() => {
-    onAuthStateChanged(auth, authenticatedUser => {
-      console.log(authenticatedUser);
-      if (authenticatedUser) {
-        setUserToken(authenticatedUser);
-      } else {
-        setUserToken(null);
-      }
-    });
-  }, []);
-
   return (
     <div className="App">
-      <Routing userToken={userToken} setUserToken={setUserToken}/>
+      <Routing />
     </div>
   );
 };
