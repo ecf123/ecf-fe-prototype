bc1f1a319d38e994c1cd8f3fe41eeb9fde5774a4
Gavin Stirling <gavinstir@gmail.com>
2022-07-21 12:53:37 +0100

    Formatting

--- src/containers/ArticleCardList/ArticleCardList.jsx
+++ src/containers/ArticleCardList/ArticleCardList.jsx
@@ -2,16 +2,26 @@ import React from "react";
 import ArticleCard from "../../components/ArticleCard/ArticleCard";
 import "./ArticleCardList.scss";
 
-const ArticleCardList = ({articleInfo}) => {
-  const articleCardJSX = articleInfo.map((article, index) => {
-    return <ArticleCard index={index} key={index} thumbnail={article.thumbnail} category={article.category} title={article.title} isActive={article.isActive} id={article.id} />;
-  });
+const ArticleCardList = ({ articleInfo }) => {
+    const articleCardJSX = articleInfo.map((article, index) => {
+        return (
+            <ArticleCard
+                index={index}
+                key={index}
+                thumbnail={article.thumbnail}
+                category={article.category}
+                title={article.title}
+                isActive={article.isActive}
+                id={article.id}
+            />
+        );
+    });
 
-  return (
-    <div className="article-card-list" data-testid="article-card-component">
-      {articleCardJSX}
-    </div>
-  );
+    return (
+        <div className="article-card-list" data-testid="article-card-component">
+            {articleCardJSX}
+        </div>
+    );
 };
 
 export default ArticleCardList;
