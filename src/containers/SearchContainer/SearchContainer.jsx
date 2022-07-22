c25ae46575b1d1c9a85f2ea520969e8344f032f0
aiman.wahizam <aiman.wahizam@nology.io>
2022-07-22 15:11:53 +0100

    deleted useless useeffect import

--- src/containers/SearchContainer/SearchContainer.jsx
+++ src/containers/SearchContainer/SearchContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import "./SearchContainer.scss";
 import ContentHeader from "../../components/ContentHeader/ContentHeader";
