0561afaa15b11c6f6af320b2628a6872caaa2732
Mark Lawson <mark@walkonmars.co.uk>
2022-07-25 15:09:42 +0100

    logout from firebase functioning

--- src/containers/Profile/Profile.jsx
+++ src/containers/Profile/Profile.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import "./Profile.scss";
 import Button from "../../components/Button/Button";
+import {auth} from "../../firebase";
+import {signOut} from "firebase/auth";
 
 // path: /profile
 
 const Profile = () => {
+  const logOut = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        console.log("You are signed out");
+      })
+      .catch((error) => {
+        // An error happened.
+        console.log(error);
+      });
+  };
+
   return (
     <div className="profile-page">
-      <Button className="profile-page__sign-out" buttonText="SIGN OUT" />
+      <Button className="profile-page__sign-out" buttonText="SIGN OUT" onClickButton={logOut} />
     </div>
   );
 };
