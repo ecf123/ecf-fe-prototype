afaeb4b4faf059d6612bf2e9c8f270d341734e08
sam <sam_pridmore@hotmail.co.uk>
2022-07-25 15:05:39 +0100

    added image to course overview, and pulled main

--- src/containers/CourseOverview/CourseOverview.jsx
+++ src/containers/CourseOverview/CourseOverview.jsx
@@ -1,16 +1,16 @@
-import React, {useState} from "react";
-import CourseOverviewList from "../CourseOverviewList/CourseOverviewList";
-import VideoCardList from "../VideoCardList/VideoCardList";
-import "./CourseOverview.scss";
-import MenuBar from "../../components/MenuBar/MenuBar";
-import BackButton from "../../components/BackButton/BackButton";
-import Navigation from "../../components/Navigation/Navigation";
-import dummyVideoData from "../../assets/data/dummyVideoCardData";
+import React, { useState } from 'react';
+import CourseOverviewList from '../CourseOverviewList/CourseOverviewList';
+import VideoCardList from '../VideoCardList/VideoCardList';
+import './CourseOverview.scss';
+import MenuBar from '../../components/MenuBar/MenuBar';
+import BackButton from '../../components/BackButton/BackButton';
+import Navigation from '../../components/Navigation/Navigation';
+import dummyVideoData from '../../assets/data/dummyVideoCardData';
 
 // path: /courses/:courseId
 
-const CourseOverview = ({dummyPathwayData}) => {
-  const [category, setCategory] = useState("lessons");
+const CourseOverview = ({ dummyPathwayData }) => {
+  const [category, setCategory] = useState('lessons');
 
   const capitalisedNames = (name) => {
     return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
@@ -23,14 +23,25 @@ const CourseOverview = ({dummyPathwayData}) => {
   return (
     <div data-testid="course-overview" className="course-overview">
       <div className="course-overview__headerSection">
-        <BackButton data-testid="back-button" linkTo="/pathways/1/skills-tree" />
-        <h1 data-testid="course-overview-heading" className="course-overview__heading">
+        <BackButton
+          data-testid="back-button"
+          linkTo="/pathways/1/skills-tree"
+        />
+        <h1
+          data-testid="course-overview-heading"
+          className="course-overview__heading"
+        >
           {dummyPathwayData[0].header}
         </h1>
       </div>
 
-      <VideoCardList data-testid="video-card" dummyVideoData={dummyVideoData} id={1} />
       <div className="course-overview__content">
+        <img
+        data-testid="overview-image"
+          className="course-overview__image"
+          src={dummyPathwayData[0].image}
+          alt=""
+        />
         <h2 className="course-overview__sub-heading" data-testid="sub-heading">
           {dummyPathwayData[0].subHeading}
         </h2>
@@ -38,11 +49,23 @@ const CourseOverview = ({dummyPathwayData}) => {
           {dummyPathwayData[0].courseIntroContent}
         </p>
         <div className="course-overview__filters">
-          <MenuBar data-testid="course-overview" link1="Lessons" link2="Challenges" link3="Additional Info" onClickLink={handleCategoryChange} />
+          <MenuBar
+            data-testid="course-overview"
+            link1="Lessons"
+            link2="Challenges"
+            link3="Additional Info"
+            onClickLink={handleCategoryChange}
+          />
         </div>
 
-        <div data-testid="course-overview-list" className="course-overview__list">
-          <CourseOverviewList title={capitalisedNames(category)} category={category} />
+        <div
+          data-testid="course-overview-list"
+          className="course-overview__list"
+        >
+          <CourseOverviewList
+            title={capitalisedNames(category)}
+            category={category}
+          />
         </div>
       </div>
       <Navigation />
