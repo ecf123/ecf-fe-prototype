b1aa76173a219afb67cefccdf7c2e7674b4dc0e0
charlie-robin <charlie.robin.richardson@gmail.com>
2022-09-07 10:33:57 +0100

    (fix):Fixed failing test

--- src/components/ContentHeader/ContentHeader.test.js
+++ src/components/ContentHeader/ContentHeader.test.js
@@ -3,38 +3,39 @@ import { render, screen } from "@testing-library/react";
 import { customRender } from "../../test-utilities/test-utilities";
 import userEvent from "@testing-library/user-event";
 import SearchContainer from "../../containers/SearchContainer/SearchContainer";
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 import PathwaysCard from "../PathwaysCard/PathwaysCard";
 import dummyPathwayData from "../../assets/data/dummyPathwayData";
+import articleData from "../../assets/data/dummyArticleCardInformation";
 
 describe("initial tests for content header", () => {
-    
-    it("should render a heading and a link on the page", () => {
-        customRender(<ContentHeader title="Test Title" link="/" />);
-        const title = screen.getByText("Test Title");
-        const link = screen.getAllByText("View All");
-        expect(title).toBeInTheDocument();
-        expect(link[0]).toBeTruthy();
-    });
+  it("should render a heading and a link on the page", () => {
+    customRender(<ContentHeader title="Test Title" link="/" />);
+    const title = screen.getByText("Test Title");
+    const link = screen.getAllByText("View All");
+    expect(title).toBeInTheDocument();
+    expect(link[0]).toBeTruthy();
+  });
 
-    it("should render a heading passed through the title prop", () => {
-        customRender(<ContentHeader title="Test Title" link="/" />);
-        const title = screen.getByText("Test Title");
-        expect(title).toBeInTheDocument();
-    });
+  it("should render a heading passed through the title prop", () => {
+    customRender(<ContentHeader title="Test Title" link="/" />);
+    const title = screen.getByText("Test Title");
+    expect(title).toBeInTheDocument();
+  });
 
-    it("should change the displayed page when 'View All' is clicked", () => {
-        //This test loads a single card when the View All link is clicked because the component to display all results has not been completed, this can be updated once the component has been made.
-        const toRender = (<Routes>
-        <Route path="/" element={<SearchContainer pathwaysLink="/test" />} />
+  it("should change the displayed page when 'View All' is clicked", () => {
+    //This test loads a single card when the View All link is clicked because the component to display all results has not been completed, this can be updated once the component has been made.
+    const toRender = (
+      <Routes>
+        <Route path="/" element={<SearchContainer pathwaysLink="/test" articleInfo={articleData} />} />
         <Route path="/test" element={<PathwaysCard card={dummyPathwayData} />} />
-        </Routes>);
+      </Routes>
+    );
 
-        customRender(toRender);
-        const viewAll = screen.getAllByText("View All");
-        userEvent.click(viewAll[0]);
-        const card = screen.getByTestId("pathwaycard");
-        expect(card).toBeInTheDocument();
-       
-    });
+    customRender(toRender);
+    const viewAll = screen.getAllByText("View All");
+    userEvent.click(viewAll[0]);
+    const card = screen.getByTestId("pathwaycard");
+    expect(card).toBeInTheDocument();
+  });
 });
