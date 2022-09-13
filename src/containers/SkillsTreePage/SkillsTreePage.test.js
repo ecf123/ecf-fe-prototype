40c47a5ec41b2163b71c3cbaff2ac83aedd46cab
charlie-robin <charlie.robin.richardson@gmail.com>
2022-09-13 12:21:36 +0100

    (test): Updating tests to use new nock data

--- src/containers/SkillsTreePage/SkillsTreePage.test.js
+++ src/containers/SkillsTreePage/SkillsTreePage.test.js
@@ -2,16 +2,22 @@ import { screen } from "@testing-library/react";
 import { customRender } from "./../../test-utilities/test-utilities";
 import userEvent from "@testing-library/user-event";
 import SkillsTreePage from "../../containers/SkillsTreePage/SkillsTreePage";
+import pathways from "../../assets/data/dummyPathwayData";
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useParams: () => ({ pathwayId: "3cAtkFTtXJzdWTYLbZk3" }),
+}));
 
 describe("Tests to check if rendered", () => {
   it("should render the title", () => {
-    customRender(<SkillsTreePage />);
+    customRender(<SkillsTreePage pathways={pathways} />);
     const title = screen.getByTestId("popup-title");
     expect(title).toBeInTheDocument();
   });
 
   it("should render the skills tree", () => {
-    customRender(<SkillsTreePage />);
+    customRender(<SkillsTreePage pathways={pathways} />);
     const skillsTree = screen.getByTestId("skills-tree");
     expect(skillsTree).toBeInTheDocument();
   });
@@ -19,8 +25,8 @@ describe("Tests to check if rendered", () => {
 
 describe("Tests for popup when clicking on a course", () => {
   it("should appear on the page", () => {
-    customRender(<SkillsTreePage />);
-    const node = screen.getByTestId("node-santander");
+    customRender(<SkillsTreePage pathways={pathways} />);
+    const node = screen.getByTestId("node-zoYyatJhKNo18hy0XJcX");
     userEvent.click(node);
     const popup = screen.getByTestId("popup");
     const overlay = screen.getByTestId("popup-overlay");
