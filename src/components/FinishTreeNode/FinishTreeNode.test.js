ee81132abd567fe406003b4400449cc15c2b355e
charlie-robin <charlie.robin.richardson@gmail.com>
2022-09-16 17:25:34 +0100

    (fix): Updated FinishNodeTree test props

--- src/components/FinishTreeNode/FinishTreeNode.test.js
+++ src/components/FinishTreeNode/FinishTreeNode.test.js
@@ -1,15 +1,15 @@
 import { screen, render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import FinishTreeNode from "./FinishTreeNode";
-import {customRender} from "./../../test-utilities/test-utilities"
-
+import { customRender } from "./../../test-utilities/test-utilities";
 
 describe("Tests to check finishTreeNode", () => {
   it("should be locked if not finished", () => {
-    render(<>
+    render(
+      <>
         <div id="parent"></div>
-        <FinishTreeNode parentId="parent" finished={false} title="hi"/>
-        </>
+        <FinishTreeNode parentId="parent" locked={true} title="hi" />
+      </>
     );
     const lock = screen.getByTestId("lock");
     const overlay = screen.getByTestId("overlay");
@@ -18,9 +18,7 @@ describe("Tests to check finishTreeNode", () => {
   });
 
   it("should ", () => {
-    render(
-        <FinishTreeNode title="test" finished={true} parentId="parentTest" />
-    );
+    render(<FinishTreeNode title="test" locked={false} parentId="parentTest" />);
     const podium = screen.getByTestId("podium");
     const line = screen.getByTestId("line");
     const heading = screen.getByTestId("heading");
